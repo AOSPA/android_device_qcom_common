c952475a9d915f5cfb2a438e4eb4dcdf09e0753f
Steve Kondik <steve@cyngn.com>
2015-10-19 14:43:39 -0700

    power: Add support for tap-to-wake feature control
    Change-Id: I545902b29f4828c127bc32def6e30b67ce4a3aa7

--- power/Android.mk
+++ power/Android.mk
@@ -47,6 +47,10 @@ ifeq ($(call is-board-platform-in-list, msm8992), true)
 LOCAL_SRC_FILES += power-8992.c
 endif
 
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
