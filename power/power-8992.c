91100af93aab37062a2908fc7e2e19ffb72eeeac
Alex Naidis <alex.naidis@linux.com>
2016-07-18 17:52:45 +0200

    power:8992/8994: modify screen off config
    * Keep all Big Cores offlined
    * The previous behaviour didn't match any of our devices power configs
    
    Change-Id: Ib46575028d191bac7fbf186592d3aab54425d468
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- power/power-8992.c
+++ power/power-8992.c
@@ -269,9 +269,8 @@ int set_interactive_override(struct power_module *module, int on)
         /* Display off */
         if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
             (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
-            // sched upmigrate = 99, sched downmigrate = 95
-            // keep the big cores around, but make them very hard to use
-            int resource_values[] = {0x4E63, 0x4F5F};
+            // Offline all big cores
+            int resource_values[] = {0x777};
             if (!display_hint_sent) {
                 perform_hint_action(DISPLAY_STATE_HINT_ID,
                 resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
