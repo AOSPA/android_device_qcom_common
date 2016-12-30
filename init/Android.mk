2b424c08cd30417df5c60bca2c82a58e18481ad7
Alex Naidis <alex.naidis@linux.com>
2016-12-30 02:44:39 +0100

    common: init: Kill board init support
    * These files were still using the old
      syntax and thus breaking build.
    * Instead of fixing all of them,
      remove support, since they have
      no use.
    
    Change-Id: I522655a8c52089dd779a131ac2a785c63856e61f
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- init/Android.mk
+++ init/Android.mk
@@ -3,8 +3,6 @@ ifeq ($(TARGET_INIT_VENDOR_LIB),libinit_msm)
 LOCAL_PATH := $(call my-dir)
 LIBINIT_MSM_PATH := $(call my-dir)
 
-LIBINIT_USE_MSM_DEFAULT := $(shell if [ ! -f "$(LIBINIT_MSM_PATH)/init_$(TARGET_BOARD_PLATFORM).cpp" ]; then echo true; fi)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
@@ -14,14 +12,8 @@ LOCAL_CPPFLAGS := \
     -Wno-unused-parameter \
     -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_msm.cpp
-ifeq ($(LIBINIT_USE_MSM_DEFAULT),true)
-  LOCAL_SRC_FILES += init_msmdefault.cpp
-else
-  ifneq ($(TARGET_LIBINIT_DEFINES_FILE),)
-    LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_DEFINES_FILE)
-  else
-    LOCAL_SRC_FILES += init_$(TARGET_BOARD_PLATFORM).cpp
-  endif
+ifneq ($(TARGET_LIBINIT_DEFINES_FILE),)
+  LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_DEFINES_FILE)
 endif
 LOCAL_MODULE := libinit_msm
 include $(BUILD_STATIC_LIBRARY)
