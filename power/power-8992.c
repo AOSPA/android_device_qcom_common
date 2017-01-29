49ba90fc3b62a8c80745c85134e92bf4f51f6e18
Alex Naidis <alex.naidis@linux.com>
2017-01-29 17:06:03 +0100

    common: power: Correct 4+0 screen off config
    Qualcomm's power hint which was supposed
    to do that is broken.
    
    This patch corrects it by replacing it with a
    mpctlv3 hint and setting the maximum big cores
    to 0 for the screen off case.
    
    The affected chipsets are msm8992 and msm8994.
    
    Change-Id: I712a13067fbbdf26c1d49d6e8229b01f0ef21b61
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- power/power-8992.c
+++ power/power-8992.c
@@ -190,7 +190,7 @@ int set_interactive_override(struct power_module *module, int on)
         /* Display off */
         if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
             (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
-            int resource_values[] = {0x777}; /* 4+0 core config in display off */
+            int resource_values[] = {0x41004000, 0x0}; /* 4+0 core config in display off */
             if (!display_hint_sent) {
                 perform_hint_action(DISPLAY_STATE_HINT_ID,
                 resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
