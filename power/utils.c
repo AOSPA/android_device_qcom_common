d6b3c52643802028f5a3e6d549320877e0ad74b7
Ananth Raghavan Subramanian <sananth@codeaurora.org>
2017-03-24 16:30:12 -0700

    power: PowerHAL for 8998
    PowerHAL now parses xml for resource list
    
    Change-Id: Ic5c2c1836e3b092e5cdc3827e323a2ffc816f5a3

--- power/utils.c
+++ power/utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2013,2015-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2013,2015-2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -241,6 +241,26 @@ int interaction_with_handle(int lock_handle, int duration, int num_args, int opt
 #endif
     return 0;
  }
+
+void release_request(int lock_handle) {
+    if (qcopt_handle && perf_lock_rel)
+        perf_lock_rel(lock_handle);
+}
+
+int interaction_with_handle(int lock_handle, int duration, int num_args, int opt_list[])
+{
+    if (duration < 0 || num_args < 1 || opt_list[0] == NULL)
+        return 0;
+
+    if (qcopt_handle) {
+        if (perf_lock_acq) {
+            lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
+            if (lock_handle == -1)
+                ALOGE("Failed to acquire lock.");
+        }
+    }
+    return lock_handle;
+}
 
 void release_request(int lock_handle) {
     if (qcopt_handle && perf_lock_rel)
