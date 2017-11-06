f96e15a9d62cbafb04b7de962c0acdee1423bd44
Bharat Pawar <bpawar@codeaurora.org>
2017-11-06 16:54:50 +0100

    common: Install video seccomp policy files in /vendor
    Vendor extended policy files are separated from the aosp
    and will now reside in /vendor
    
    Change-Id: I807484b5a5d69db81db4684adb91c0795ba88673

--- common.mk
+++ common.mk
@@ -30,6 +30,11 @@ TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
 
+# video seccomp policy files
+PRODUCT_COPY_FILES += \
+    device/qcom/common/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    device/qcom/common/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Advanced DPM
 ifeq ($(TARGET_WANTS_EXTENDED_DPM_PLATFORM),true)
 PRODUCT_BOOT_JARS += tcmclient
