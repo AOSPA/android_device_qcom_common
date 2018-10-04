85b6e4deb5f6fda325e9402e1109fd852e8f5b97
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2018-10-04 10:37:23 +0530

    base.mk: Add MSMSTEPPE thermal HAL module to base.mk
    Add target specific thermal HAL module for MSMSTEPPE to
    PRODUCT_PACKAGES.
    
    Change-Id: Id9adf23ebfa21ccb5fca2bb6c7c3c2e3cef9bf59

--- base.mk
+++ base.mk
@@ -730,6 +730,7 @@ THERMAL_HAL += thermal.msm8996
 THERMAL_HAL += thermal.msm8953
 THERMAL_HAL += thermal.msm8937
 THERMAL_HAL += thermal.msmnile
+THERMAL_HAL += thermal.$(MSMSTEPPE)
 
 #TSLIB_EXTERNAL
 TSLIB_EXTERNAL := corgi
