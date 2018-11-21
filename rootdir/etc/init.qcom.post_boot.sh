870643eaff65929a03a0afe10bc478a42cf38053
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-11-21 12:31:56 +0530

    init.qcom.post_boot.sh: start cdsprpcd service only for sdm660
    Start service cdsprpcd for sdm660 with soc_id 317 only and make it
    disabled by default.
    
    Change-Id: Ibd078d9ad0d529724f37d559b0939f439e6e1ee2

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2500,7 +2500,9 @@ case "$target" in
             echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
             # Start cdsprpcd only for sdm660 and disable for sdm630
-            start vendor.cdsprpcd
+            if [ "$soc_id" -eq "317" ]; then
+                start vendor.cdsprpcd
+            fi
 
             # Start Host based Touch processing
                 case "$hw_platform" in
