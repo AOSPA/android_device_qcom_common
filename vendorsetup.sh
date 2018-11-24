9cccea5adf936a520cf7c5c8d09b1523987832bc
Josh Choo <dev@joshchoo.com>
2018-11-24 03:40:13 +0000

    common: sdclang bringup
    Change-Id: I9f4c535c4fb7cee6b63b9f8f83fbd3be6d12d2e3
    Signed-off-by: Josh Choo <dev@joshchoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -65,3 +65,8 @@ add_lunch_combo qcs605-userdebug
 add_lunch_combo talos-userdebug
 add_lunch_combo talos_au-userdebug
 add_lunch_combo trinket-userdebug
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=device/qcom/common/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=device/qcom/common/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false
