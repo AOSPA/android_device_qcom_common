ce7134ef14ef3aec140b05f5f256a21e9594af63
Hernán Castañón Álvarez <herna@paranoidandroid.co>
2019-07-27 13:47:06 +0200

    common: utils: add msmnile, trinket & sm6150
    Signed-off-by: Hernán Castañón Álvarez <herna@paranoidandroid.co>
    Change-Id: I1254567866b148c83ebdaa3389092cac992d5d7b

--- utils.mk
+++ utils.mk
@@ -20,12 +20,15 @@ QCOM_BOARD_PLATFORMS += apq8084
 QCOM_BOARD_PLATFORMS += sdm660
 QCOM_BOARD_PLATFORMS += sdm710
 QCOM_BOARD_PLATFORMS += sdm845
+QCOM_BOARD_PLATFORMS += msmnile
+QCOM_BOARD_PLATFORMS += trinket
+QCOM_BOARD_PLATFORMS += sm6150
 
 #List of targets that use video hw
-MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 sdm660 sdm710 sdm845
+MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 msm8937 msm8953 msm8998 sdm660 sdm710 sdm845 msmnile trinket sm6150
 
 #List of targets that use master side content protection
-MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm710 sdm845
+MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm710 sdm845 msmnile trinket sm6150
 
 # vars for use by utils
 empty :=
