91089bb4736399f19abeeec29147dab4d6fc6b41
Jake Weinstein <jake@aospa.co>
2019-07-29 16:48:32 -0500

    common: remove deprecated DPM configuration
    Change-Id: I84d706b1adf72c4f9c36bbdb87476186fff328ed

--- common.mk
+++ common.mk
@@ -16,11 +16,3 @@ BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
 PRODUCT_COPY_FILES += \
     device/qcom/common/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     device/qcom/common/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
-# Advanced DPM
-ifeq ($(TARGET_WANTS_EXTENDED_DPM_PLATFORM),true)
-PRODUCT_BOOT_JARS += tcmclient
-PRODUCT_BOOT_JARS += com.qti.dpmframework
-PRODUCT_BOOT_JARS += dpmapi
-PRODUCT_BOOT_JARS += com.qti.location.sdk
-endif
