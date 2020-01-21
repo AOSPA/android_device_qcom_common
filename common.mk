7dce3a1e746836d3cee930a8ad89af66cc0400ae
Jake Weinstein <jake@aospa.co>
2020-01-21 07:23:20 +0100

    common: automatically define AOSP and QCOM_BSP flags by SoC
    Change-Id: I8e457b7e14d33080eed8fb7bfb0a9d23f3bf4959

--- common.mk
+++ common.mk
@@ -1,3 +1,6 @@
+# include additional build utilities
+include device/qcom/common/utils.mk
+
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_HAS_QC_KERNEL_SOURCE := true
 TARGET_USES_QCOM_MM_AUDIO := true
@@ -8,11 +11,18 @@ BOARD_USES_QCNE := true
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
+# Set TARGET_USES_AOSP per platform following Qualcomm.
+ifeq ($(call is-board-platform-in-list, msm8996 sdm660),true)
+TARGET_USES_AOSP ?= true
+else
+TARGET_USES_AOSP ?= false
+endif
+
+TARGET_USES_AOSP_FOR_AUDIO ?= false
+TARGET_USES_QCOM_BSP ?= false
+
 #skip boot jars check
 SKIP_BOOT_JARS_CHECK := true
-
-# include additional build utilities
-include device/qcom/common/utils.mk
 
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
