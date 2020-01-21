6c0c52b7a822158153a540f6b84ac7c943dd4338
Jake Weinstein <jake@aospa.co>
2020-01-21 07:23:20 +0100

    common: automatically define AOSP and QCOM_BSP flags by SoC
    Change-Id: I8e457b7e14d33080eed8fb7bfb0a9d23f3bf4959

--- common.mk
+++ common.mk
@@ -8,6 +8,15 @@ BOARD_USES_QCNE := true
 
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
+ifneq ($(call is-board-platform-in-list, msm8996 sdm660), true)
+TARGET_USES_AOSP ?= false
+else
+TARGET_USES_AOSP ?= true
+endif
+
+TARGET_USES_AOSP_FOR_AUDIO ?= false
+TARGET_USES_QCOM_BSP ?= false
+
 #skip boot jars check
 SKIP_BOOT_JARS_CHECK := true
 
