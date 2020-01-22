af8bfddb41a6367f63ee6f7fb1b766ca8d8168a5
Jake Weinstein <jake@aospa.co>
2020-01-22 01:54:20 +0100

    common: include QC split Makefiles and set default flags
    This allows us to clean up device trees and automatically
    stay up to date with Qualcomm's Makefile changes.
    
    Change-Id: Idc7b525bb014bf0e4bb58375e81254b125872d48

--- common.mk
+++ common.mk
@@ -1,24 +1,61 @@
 # include additional build utilities
 include device/qcom/common/utils.mk
 
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_HAS_QC_KERNEL_SOURCE := true
-TARGET_USES_QCOM_MM_AUDIO := true
-
-BOARD_USES_ADRENO := true
-
-BOARD_USES_QCNE := true
-
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
+# Set TARGET_USES_AOSP per platform following Qualcomm.
 ifneq ($(call is-board-platform-in-list, msm8996 sdm660), true)
 TARGET_USES_AOSP ?= false
 else
 TARGET_USES_AOSP ?= true
 endif
 
+# Audio
 TARGET_USES_AOSP_FOR_AUDIO ?= false
+TARGET_USES_QCOM_MM_AUDIO := true
+-include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/qssi/qssi.mk
+AUDIO_FEATURE_ENABLED_3D_AUDIO ?= false
+
+# Bluetooth
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+ifneq ($(call is-board-platform-in-list, msm8937 msm8953 msm8996 msm8998 sdm660), true)
+# Recent Platform
+-include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
+else
+# Legacy Platform
+-include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-legacy-board.mk
+endif
+# If BLUETOOTH_QCOM and !QTI_BT_STACK
+elseif ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+-include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-legacy-aosp-board.mk
+endif
+# For all devices with QCOM BT
+ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
+include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk
+endif
+
+# Display
+BOARD_USES_ADRENO := true
+TARGET_USES_ION := true
 TARGET_USES_QCOM_BSP ?= false
+-include hardware/qcom/display/config/display-board.mk
+$(call inherit-product-if-exists, hardware/qcom/display/display-product.mk)
+$(call inherit-product-if-exists, vendor/qcom/opensource/commonsys-intf/display/display-interfaces-product.mk)
+$(call inherit-product-if-exists, vendor/qcom/opensource/commonsys-intf/display/display-product-system.mk)
+
+# Kernel
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# Media
+TARGET_ENABLE_MEDIADRM_64 := true
+$(call inherit-product-if-exists, hardware/qcom/media/product.mk)
+
+# Power
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+-include vendor/qcom/opensource/power/power-vendor-board.mk
+$(call inherit-product-if-exists, vendor/qcom/opensource/power/power-vendor-product.mk)
+endif
 
 #skip boot jars check
 SKIP_BOOT_JARS_CHECK := true
+
+# Wifi
+DISABLE_EAP_PROXY := true
