beb399638b8778a27611da8f39c7c616c0ce9d91
Jake Weinstein <jake@aospa.co>
2020-01-23 05:35:39 +0100

    common: remove seccomp from here
    We'll inherit this from Media HAL.
    
    Change-Id: I91042cd0519c6c7494489061d80b8067868e21d8

--- common.mk
+++ common.mk
@@ -23,11 +23,3 @@ TARGET_USES_QCOM_BSP ?= false
 
 #skip boot jars check
 SKIP_BOOT_JARS_CHECK := true
-
-# SECCOMP Extension
-BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
-
-# video seccomp policy files
-PRODUCT_COPY_FILES += \
-    device/qcom/common/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    device/qcom/common/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
