d808abd9c818852bcb2d2000758e9f9fdefd621d
Jake Weinstein <jake@aospa.co>
2020-03-09 03:32:08 +0100

    common: inherit Qualcomm PowerHAL Makefiles
    This automatically defines the PowerHAL modules.
    
    By default, SoCs that use HAL 1.2 on stock do the same here,
    while legacy SoCs use 1.0. If you define TARGET_USES_NON_LEGACY_POWERHAL
    on a legacy SoC, it will enable 1.2.
    
    Change-Id: If70849e3ea23cb9772a053f6b227465541ee6b94

--- common.mk
+++ common.mk
@@ -8,6 +8,11 @@ BOARD_USES_ADRENO := true
 
 TARGET_USES_AOSP_FOR_AUDIO ?= false
 
+# Power
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+$(call inherit-product-if-exists, vendor/qcom/opensource/power/power-vendor-product.mk)
+endif
+
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
 
