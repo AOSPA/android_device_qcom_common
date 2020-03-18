beb4a2d4b9ecf6bc984b92a39c4b474e90e49276
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:35:44 +0530

    common: Add common QTI components
     - To use this, set TARGET_COMMON_QTI_COMPONENTS := <components>.
       e.g. TARGET_COMMON_QTI_COMPONENTS := av bt perf
     - Configurations go inside qti-components/<component>/qti-<component>.mk
     - Blob list go in qti-components/<component>/proprietary-files.txt
    
    Change-Id: I9868b02da59c69326f3587cf338187753955c1db

--- common.mk
+++ common.mk
@@ -1,5 +1,23 @@
+#
+# Copyright 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/qcom/common
+
 # include additional build utilities
-include device/qcom/common/utils.mk
+include $(DEVICE_PATH)/utils.mk
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_USES_QCOM_MM_AUDIO := true
@@ -7,3 +25,24 @@ TARGET_USES_QCOM_MM_AUDIO := true
 BOARD_USES_ADRENO := true
 
 TARGET_USES_AOSP_FOR_AUDIO ?= false
+
+# QTI common components
+ifneq (,$(filter av, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/av/qti-av.mk
+endif
+
+ifneq (,$(filter bt, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/bt/qti-bt.mk
+endif
+
+ifneq (,$(filter perf, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/perf/qti-perf.mk
+endif
+
+ifneq (,$(filter telephony, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/telephony/qti-telephony.mk
+endif
+
+ifneq (,$(filter wfd, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/wfd/qti-wfd.mk
+endif
