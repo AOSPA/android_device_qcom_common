4cfb0a59d8c11ebc25b4d1fce629daf71196b982
Jake Weinstein <jake@aospa.co>
2020-03-19 04:43:51 +0100

    common: move seccomp to vendor/pa
    Change-Id: Ifd24850a786c336d7529317a53bdbd912cdecd58

--- common.mk
+++ common.mk
@@ -7,11 +7,3 @@ TARGET_USES_QCOM_MM_AUDIO := true
 BOARD_USES_ADRENO := true
 
 TARGET_USES_AOSP_FOR_AUDIO ?= false
-
-# SECCOMP Extension
-BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
-
-# video seccomp policy files
-PRODUCT_COPY_FILES += \
-    device/qcom/common/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    device/qcom/common/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
