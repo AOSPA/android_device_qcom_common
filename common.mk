ec66fdafbbe03850c5a995afbdb02f695d996a9c
TheMalachite <fastretina@gmail.com>
2020-03-25 00:26:19 +0100

    common: build required QTI HIDL Wrapper, telephony utils and VNDK Framework Detect
    Change-Id: I6cbfdfc1aba636936446a548589e28d34f312c02

--- common.mk
+++ common.mk
@@ -16,22 +16,6 @@
 
 DEVICE_PATH := device/qcom/common
 
-# include additional build utilities
-include $(DEVICE_PATH)/utils.mk
-
-TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_USES_QCOM_MM_AUDIO := true
-
-BOARD_USES_ADRENO := true
-
-TARGET_USES_AOSP_FOR_AUDIO ?= false
-
-# Camera
-# Enable Media Extensions for HAL1 on Legacy Devices
-ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660),true)
-  TARGET_USES_MEDIA_EXTENSIONS := true
-endif
-
 # QTI common components
 ifneq (,$(filter av, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/av/qti-av.mk
@@ -52,3 +36,10 @@ endif
 ifneq (,$(filter wfd, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/wfd/qti-wfd.mk
 endif
+
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
