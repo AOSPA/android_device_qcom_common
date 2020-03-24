e72e9841768f744ae82421e96d00cd0d8b82f224
TheMalachite <fastretina@gmail.com>
2020-03-25 00:26:19 +0100

    common: build required HIDL, IMS, telephony-ext packages, boot jars and QTI telephony packages
    Change-Id: I6cbfdfc1aba636936446a548589e28d34f312c02

--- common.mk
+++ common.mk
@@ -8,6 +8,32 @@ BOARD_USES_ADRENO := true
 
 TARGET_USES_AOSP_FOR_AUDIO ?= false
 
+# HIDL Wrapper
+PRODUCT_PACKAGES += \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml
+
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml
+
+# MSIM manual provisioning
+PRODUCT_PACKAGES += telephony-ext
+PRODUCT_BOOT_JARS += telephony-ext
+
+# QTI Telephony Utils
+PRODUCT_PACKAGES += \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
+
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
+
 # SECCOMP Extension
 BOARD_SECCOMP_POLICY += device/qcom/common/seccomp
 
