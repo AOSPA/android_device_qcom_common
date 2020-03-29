f89fadb66b92908a3e23a031068f4561f2636f4c
Jake Weinstein <jake@aospa.co>
2020-03-29 17:50:00 +0200

    common: Conditionally build Qualcomm PowerHAL
    - Legacy devices use legacy (1.0) PowerHAL by default but can opt
      in to the binderized (1.2) PowerHAL by setting TARGET_USES_NON_LEGACY_POWERHAL.
    
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I78f24f16d7454d3cdbea6751b826f6d2badb35de

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -27,6 +27,16 @@ ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 
   TARGET_USES_MEDIA_EXTENSIONS := true
 endif
 
+# Power
+# Disable Binderized Power HAL by default for legacy targets.
+# Devices can still opt in by setting TARGET_USES_NON_LEGACY_POWERHAL in BoardConfig.mk.
+# Conversely, recent chips, such as sm8150 with an old vendor can opt out.
+ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+ifneq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660 sdm710 sdm845 trinket),true)
+TARGET_USES_NON_LEGACY_POWERHAL ?= true
+endif
+endif
+
 # SEPolicy
 # Boards can opt-out of QCOM sepolicy
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
