6b48197189c238f7182e0ecda796a5914391fbb9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-29 11:20:20 +0530

    common: Move media extensions flag to vendor board config
    - There is no point in adding platform-specific config here
      cuz BoardConfig is read only after pa_device.mk device.mk so
      at this point TARGET_BOARD_PLATFORM is null
    
    Change-Id: Ife96c4c95bb764d08d1c3d0fed1cf2921a58390b

--- common.mk
+++ common.mk
@@ -26,12 +26,6 @@ BOARD_USES_ADRENO := true
 
 TARGET_USES_AOSP_FOR_AUDIO ?= false
 
-# Camera
-# Enable Media Extensions for HAL1 on Legacy Devices
-ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660),true)
-  TARGET_USES_MEDIA_EXTENSIONS := true
-endif
-
 # QTI common components
 ifneq (,$(filter av, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/av/qti-av.mk
