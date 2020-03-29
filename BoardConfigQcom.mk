7fa82281ddf005d9e64928e6c323b03054e77299
Jake Weinstein <jake@aospa.co>
2020-03-29 17:35:08 +0200

    common: Enable TARGET_USES_MEDIA_EXTENSIONS on legacy SoCs
    This needs to be in BoardConfig so the SoC can be detected.  We will
    also move all board flags to this file in subsequent commits.
    
    Change-Id: I5e60c482a18b480fca9a77fabc9f815413e494d3

---
+++ BoardConfigQcom.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Enable Media Extensions for HAL1 on Legacy Devices
+ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8937 msm8952 msm8953 msm8960 msm8974 msm8976 msm8992 msm8994 msm8996 msm8998 sdm660),true)
+  TARGET_USES_MEDIA_EXTENSIONS := true
+endif
