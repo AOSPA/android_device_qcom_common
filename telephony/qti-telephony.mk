1034ddef33e4244a54e01cda98c2a4b5ee706b97
Jake Weinstein <jake@aospa.co>
2020-04-01 02:54:49 +0200

    common: telephony: Build Radio HALs
    Change-Id: I2bb2361e200f27191d15505094361ea765142ff8

--- telephony/qti-telephony.mk
+++ telephony/qti-telephony.mk
@@ -14,5 +14,15 @@
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/qcom/common/telephony/framework_manifest.xml
 
+# Radio
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.deprecated@1.0
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/telephony/telephony-vendor.mk)
