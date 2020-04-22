6956d64a457dbaa4f4e5591005c5d89fb73ac682
Alexander Koskovich <zvnexus@outlook.com>
2020-04-22 17:29:52 -0400

    Update radio configuration to v1.2.
    Change-Id: I1bae448fc97664493c5969d6f02bb2447af85154

--- telephony/qti-telephony.mk
+++ telephony/qti-telephony.mk
@@ -17,7 +17,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += device/qcom/common/telephony/framework_manifes
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.2 \
     android.hardware.radio.deprecated@1.0
 
 # Get non-open-source specific aspects
