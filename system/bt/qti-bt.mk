b2a90e4be4be5a2b2e8f1476f1feed5a32f71be7
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 11:15:11 +0800

    bt: Disable BT A2DP AAC whitelist
    * Results in some supposedly AAC compatible devices
      like the Samsung Galaxy Buds Live not having it as
      an option despite being a supported codec of the
      device.
    
    Change-Id: Ia53a378c74c9cd5ec0d881026b2067aa4d638fcb

--- system/bt/qti-bt.mk
+++ system/bt/qti-bt.mk
@@ -21,7 +21,8 @@ endif
 # Properties
 ifneq ($(TARGET_USE_QTI_BT_STACK),false)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.bluetooth.library_name=libbluetooth_qti.so
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
 endif
 
 # Get non-open-source specific aspects.
