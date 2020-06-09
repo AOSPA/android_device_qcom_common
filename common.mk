e8640aa541e458c146adbea488e61a5a12c852fa
Jake Weinstein <jake@aospa.co>
2020-06-09 06:29:55 +0200

    common: Introduce Display Component
    Change-Id: I0853820ab7a52aa1dc7a47d3576d3dc96f451b50

--- common.mk
+++ common.mk
@@ -29,6 +29,10 @@ ifneq (,$(filter bt, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/bt/qti-bt.mk
 endif
 
+ifneq (,$(filter display, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/display/qti-display.mk
+endif
+
 ifneq (,$(filter perf, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/perf/qti-perf.mk
 endif
