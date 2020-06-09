4ca6aa61853e8dda9ebc8b7a500d4d5bcd34fe17
Jake Weinstein <jake@aospa.co>
2020-06-09 06:29:55 +0200

    common: Introduce Display Component for QSSI SoCs (845 and newer)
    Change-Id: I0853820ab7a52aa1dc7a47d3576d3dc96f451b50

---
+++ display/qti-display.mk
@@ -0,0 +1,20 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include Display HAL Makefiles
+include hardware/qcom/display/config/display-product.mk
+include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
+
+# Get non-open-source specific aspects
+$(call inherit-product-if-exists, vendor/qcom/common/display/display-vendor.mk)
