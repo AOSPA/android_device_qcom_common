83a41de022afd54447ba8ce2e38dd8ded0257bb1
Alexander Koskovich <zvnexus@outlook.com>
2020-08-25 15:25:43 +0000

    Add an initialization component from LA.UM.9.1.r1-08400-SMxxx0.0.
    Change-Id: I49dbed09776c6d8437c4a749ee78849a5d8206e6

---
+++ init/qti-init.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/init
+
+# Init
+PRODUCT_PACKAGES += \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.factory.rc \
+    init.qcom.post_boot.sh \
+    init.qcom.rc \
+    init.qcom.sdio.sh \
+    init.qcom.sh \
+    init.qti.can.sh \
+    init.recovery.qcom.rc \
+    qca6234-service.sh \
+    ueventd.qcom.rc
