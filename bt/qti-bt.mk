82db0076cf165f1a4a715f70c7668fe525aabc57
Anay Wadhera <awadhera@berkeley.edu>
2020-10-21 17:21:07 -0700

    common: Add vendor service/libraries to BT component.
    Change-Id: Id3e3eb7d20186898cb041e7aa43f58ac8ad0124c

--- bt/qti-bt.mk
+++ bt/qti-bt.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/bt
+
 # Include QTI Bluetooth makefiles.
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
@@ -20,6 +23,25 @@ $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-syste
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service-qti.xml \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    com.dsi.ant@1.0 \
+    com.dsi.ant@1.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0 \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
+    vendor.qti.hardware.bluetooth_audio@2.0 \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0 \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.bluetooth.wipower=false
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/bt/bt-vendor.mk)
