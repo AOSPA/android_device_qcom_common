5510eef799aa5960efe382b29251897d40ffb92d
Alexander Koskovich <zvnexus@outlook.com>
2020-11-09 10:56:53 -0700

    common: Create a media component.
    Change-Id: I3b564a20637c8a1ca4b4e54d829201d0d51f2561

--- common.mk
+++ common.mk
@@ -117,6 +117,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     display \
     gps \
     init \
+    media \
     overlay \
     perf \
     telephony \
@@ -138,6 +139,10 @@ endif
 
 ifneq (,$(filter init, $(TARGET_COMMON_QTI_COMPONENTS)))
 include $(DEVICE_PATH)/init/qti-init.mk
+endif
+
+ifneq (,$(filter media, $(TARGET_COMMON_QTI_COMPONENTS)))
+include $(DEVICE_PATH)/media/qti-media.mk
 endif
 
 ifneq (,$(filter overlay, $(TARGET_COMMON_QTI_COMPONENTS)))
