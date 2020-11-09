e9a55856616f1c48e7b892297f1a5e87e6ea3c43
Alexander Koskovich <zvnexus@outlook.com>
2020-11-09 11:24:09 -0700

    common: Create a WLAN component.
     * Only pick up product makefile if it exists, some
       vendorless devices may be opting into all components.
    
    Change-Id: I16d49283f754e2c3dd1743ca85f3e625e8496e8f

---
+++ wlan/qti-wlan.mk
@@ -0,0 +1,55 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Error if TARGET_BOARD_PLATFORM is not set because otherwise the modules with the board in the name cannot be built.
+ifeq ($(TARGET_BOARD_PLATFORM),)
+$(error "TARGET_BOARD_PLATFORM is not defined yet. Please define in your device Makefile if you wish to use this component")
+endif
+
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    fstman \
+    fstman.ini \
+    hostapd \
+    hostapd.accept \
+    hostapd.deny \
+    hostapd_cli \
+    hostapd_default.conf \
+    libqsap_sdk \
+    libwifi-hal-qcom \
+    sigma_dut \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# IPACM
+$(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
+
+# Include QCOM WLAN makefiles.
+-include device/qcom/wlan/$(TARGET_BOARD_PLATFORM)/BoardConfigWlan.mk
+-include device/qcom/wlan/$(TARGET_BOARD_PLATFORM)/wlan.mk
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/wlan/wlan-vendor.mk)
