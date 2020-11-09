8843924acf27d41c40ff71690e4b410924b3f8c6
Alexander Koskovich <zvnexus@outlook.com>
2020-11-09 10:49:50 -0700

    common: Import QCOM board utilities from LA.UM.9.12.r1-08900-SMxx50.0.
    Change-Id: I77eee7bc8892d836e14564d99b13250b426ead35

--- common.mk
+++ common.mk
@@ -16,6 +16,90 @@
 
 DEVICE_PATH := device/qcom/common
 
+# List of QCOM targets.
+MSMSTEPPE := sm6150
+TRINKET := trinket
+
+QCOM_BOARD_PLATFORMS += \
+    $(MSMSTEPPE) \
+    $(TRINKET) \
+    apq8084 \
+    apq8098_latv \
+    atoll \
+    bengal \
+    kona \
+    lito \
+    mpq8092 \
+    msm8226 \
+    msm8610 \
+    msm8909 \
+    msm8909_512 \
+    msm8916 \
+    msm8916_32 \
+    msm8916_32_512 \
+    msm8916_64 \
+    msm8937 \
+    msm8952 \
+    msm8953 \
+    msm8974 \
+    msm8992 \
+    msm8994 \
+    msm8996 \
+    msm8998 \
+    msmnile \
+    msmnile_au \
+    msm_bronze \
+    qcs605 \
+    sdm660 \
+    sdm710 \
+    sdm845
+
+# List of targets that use video hardware.
+MSM_VIDC_TARGET_LIST := \
+    $(MSMSTEPPE) \
+    $(TRINKET) \
+    apq8084 \
+    apq8098_latv \
+    atoll \
+    kona \
+    lito \
+    msm8226 \
+    msm8610 \
+    msm8909 \
+    msm8916 \
+    msm8937 \
+    msm8952 \
+    msm8953 \
+    msm8974 \
+    msm8992 \
+    msm8994 \
+    msm8996 \
+    msm8998 \
+    msmnile \
+    qcs605 \
+    sdm660 \
+    sdm710 \
+    sdm845
+
+# List of targets that use master side content protection.
+MASTER_SIDE_CP_TARGET_LIST := \
+    $(MSMSTEPPE) \
+    $(TRINKET) \
+    apq8098_latv \
+    atoll \
+    bengal \
+    holi \
+    kona \
+    lito \
+    msm8996 \
+    msm8998 \
+    msmnile \
+    qcs605 \
+    sdm660 \
+    sdm710 \
+    sdm845
+
+include $(DEVICE_PATH)/utils.mk
 
 # Power
 # Define all modules and they will be filtered out
