b2023bb9a158e26c46946c5d99536453a2290626
Alexander Koskovich <zvnexus@outlook.com>
2020-11-09 11:24:09 -0700

    common: Create a WLAN component.
     * Only pick up product makefile if it exists, some
       vendorless devices may be opting into all components.
    
     * Move BoardConfigWlan flags here as CAF's tries to
       include kernel modules that don't exist and this
       is a better solution than touching all of their
       BoardConfigs.
    
    Change-Id: I16d49283f754e2c3dd1743ca85f3e625e8496e8f

--- telephony/qti-telephony.mk
+++ telephony/qti-telephony.mk
@@ -14,11 +14,11 @@
 
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/qcom/common/telephony/framework_manifest.xml
 
+# IPACM
+$(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
+
 # Dependencies
 $(call inherit-product-if-exists, device/qcom/common/telephony-diag/qti-telephony-diag.mk)
-
-# Radio
-$(call inherit-product-if-exists, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
 
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
@@ -27,5 +27,5 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0 \
     librmnetctl
 
-# Get non-open-source specific aspects
+# Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/telephony/telephony-vendor.mk)
