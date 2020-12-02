457e1b4ecc34eab42dfd844140ff2e6708975814
Alexander Koskovich <zvnexus@outlook.com>
2020-12-02 12:19:46 -0700

    common: Split common into component specific makefile.
    Change-Id: I1b705598d88556f62076fb2f4f2d4f69a8f5dbd5

--- common.mk
+++ common.mk
@@ -1,4 +1,3 @@
-#
 # Copyright 2020 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 DEVICE_PATH := device/qcom/common
 
@@ -160,89 +158,3 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(DEVICE_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(DEVICE_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
-ifeq ($(TARGET_COMMON_QTI_COMPONENTS), all)
-TARGET_COMMON_QTI_COMPONENTS := \
-    audio \
-    av \
-    bt \
-    display \
-    gps \
-    init \
-    media \
-    nq-nfc \
-    overlay \
-    perf \
-    telephony \
-    usb \
-    vibrator \
-    wfd \
-    wlan
-endif
-
-# QTI common components
-ifneq (,$(filter av, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/av/qti-av.mk
-endif
-
-ifneq (,$(filter bt, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/bt/qti-bt.mk
-endif
-
-ifneq (,$(filter gps, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/gps/qti-gps.mk
-endif
-
-ifneq (,$(filter init, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/init/qti-init.mk
-endif
-
-ifneq (,$(filter overlay, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/overlay/qti-overlay.mk
-endif
-
-ifneq (,$(filter perf, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/perf/qti-perf.mk
-endif
-
-ifneq (,$(filter telephony, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/telephony/qti-telephony.mk
-endif
-
-ifneq (,$(filter usb, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/usb/qti-usb.mk
-endif
-
-ifneq (,$(filter vibrator, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/vibrator/qti-vibrator.mk
-endif
-
-# 845 series and newer
-ifneq (,$(filter audio, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/audio/qti-audio.mk
-endif
-
-ifneq (,$(filter display, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/display/qti-display.mk
-endif
-
-ifneq (,$(filter media, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/media/qti-media.mk
-endif
-
-ifneq (,$(filter nq-nfc, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/nq-nfc/qti-nq-nfc.mk
-endif
-
-ifneq (,$(filter wfd, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/wfd/qti-wfd.mk
-endif
-
-ifneq (,$(filter wlan, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/wlan/qti-wlan.mk
-endif
-
-# 8998 series and older
-ifneq (,$(filter wfd-legacy, $(TARGET_COMMON_QTI_COMPONENTS)))
-include $(DEVICE_PATH)/wfd-legacy/qti-wfd-legacy.mk
-endif
