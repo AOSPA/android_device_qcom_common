18146ffcb4cfe47bff12ee33c17c9bd7ce915a86
Vishalcj17 <vishalcj@aospa.co>
2020-12-05 23:36:40 +0530

    USB: Build android.hardware.usb@1.0 for 4.14 and 4.9 devices.
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I9a806aae22774ef9333910b73c05f968d8e195a1

--- usb/qti-usb.mk
+++ usb/qti-usb.mk
@@ -15,6 +15,10 @@
 # Inherit from the USB product definition.
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
 
+ifeq ($(TARGET_KERNEL_VERSION), 4.14 4.9)
+PRODUCT_PACKAGES += android.hardware.usb@1.0-service
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
