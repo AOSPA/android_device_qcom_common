aedfc2970f3c1cd24f3586651d0cd9841cb466f3
Vishalcj17 <vishalcj@aospa.co>
2020-12-06 12:56:42 +0530

    gps: Inherit GPS HAL and copy permissions.
    Change-Id: I728979366e7fa27ef2f6379eeb989dc328b62c7c

--- gps/qti-gps.mk
+++ gps/qti-gps.mk
@@ -12,9 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Flags
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+
+# Inherit the GPS HAL.
+$(call inherit-product-if-exists, hardware/qcom/gps/gps_vendor_product.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     QCOMGPSFrameworksOverlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/gps/gps-vendor.mk)
