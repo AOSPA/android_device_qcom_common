97cb57ecdc433bab98d7b83300050076f7ae79e1
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-01-30 15:04:19 +0000

    common: Use Soong namespace to avoid device build conflicts
    * This prevents module duplication, and allows devices to declare
      their own versions of these modules.
    
    * Has the added nicety of allowing this repo to exist gracefully in
      other non-PA source trees.
    
    Change-Id: Idec7b4cfeed4241f5ae0ee9ffca8dc35de7735c2

--- overlay/qti-overlay.mk
+++ overlay/qti-overlay.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/overlay
+
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
