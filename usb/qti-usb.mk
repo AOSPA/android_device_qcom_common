75ab3c9a3cb4cb5a98ef110f3cd906dcb50d74e3
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-01-30 15:04:19 +0000

    common: Use Soong namespace to avoid device build conflicts
    * This prevents module duplication, and allows devices to declare
      their own versions of these modules.
    
    * Has the added nicety of allowing this repo to exist gracefully in
      other non-PA source trees.
    
    Change-Id: Idec7b4cfeed4241f5ae0ee9ffca8dc35de7735c2

--- usb/qti-usb.mk
+++ usb/qti-usb.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Paranoid Android
+# Copyright (C) 2021 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/usb
 
 # Inherit from the USB product definition.
 $(call inherit-product, vendor/qcom/opensource/usb/vendor_product.mk)
