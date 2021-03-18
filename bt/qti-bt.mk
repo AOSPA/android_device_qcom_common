1b6227ff5c981ec23fae3b13483573b00b46671e
Alexander Koskovich <zvnexus@outlook.com>
2021-03-18 15:58:28 -0700

    common: bt: Set the default BT library to QTI.
    Change-Id: If009d1d4da840ac76064859949bfb2ab44ca72a1

--- bt/qti-bt.mk
+++ bt/qti-bt.mk
@@ -23,5 +23,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+# Properties
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.bluetooth.library_name=libbluetooth_qti.so
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/bt/bt-vendor.mk)
