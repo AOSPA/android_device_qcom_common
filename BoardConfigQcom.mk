4133157670d9c8fffcdd954e692383388d1710fd
Vishalcj17 <vishalcj@aospa.co>
2021-03-24 11:27:49 +0530

    BoardConfig: Don't include if target specifies the use of legacy sepolicy.
    Change-Id: Ib0ca729c1601b783e3e7613e0a76e6afde5f7cb6

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -33,6 +33,7 @@ endif
 TARGET_MOUNT_POINTS_SYMLINKS ?= false
 
 # SEPolicy
+ifneq ($(TARGET_USES_LEGACY_SEPOLICY),false)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
@@ -40,3 +41,4 @@ else
 include device/qcom/sepolicy/SEPolicy.mk
 endif
 endif # Exclude QCOM SEPolicy
+endif
