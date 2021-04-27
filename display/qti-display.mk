6797fef321ce5f9a0ef2b8437c576bb9002f31d3
Alexander Koskovich <zvnexus@outlook.com>
2021-04-27 04:54:44 -0700

    common: display: Build the QTI lights HALs.
     * Devices that need to have a custom one in tree should
       override these targets.
    
    Change-Id: Ic88da882cee849bbfe80f7de330dd319b17f7e8d

--- display/qti-display.mk
+++ display/qti-display.mk
@@ -26,9 +26,11 @@ PRODUCT_COPY_FILES += \
 
 # Packages
 PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti \
     libqdutils \
     libqservice \
-    libtinyxml
+    libtinyxml \
+    lights.qcom
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/display/display-vendor.mk)
