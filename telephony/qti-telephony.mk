171536c9364a0201b996969de33d5c06fd6adcc0
Alexander Koskovich <zvnexus@outlook.com>
2021-05-06 19:41:57 -0700

    common: telephony: Make the embmssl prebuilt HIDL interfaces common.
    Change-Id: Icb1d1ab8e162abbc5b3c039584eae52bd9a2e0b9

--- telephony/qti-telephony.mk
+++ telephony/qti-telephony.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/telephony
+
 # Data Services
 $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_product.mk)
 
