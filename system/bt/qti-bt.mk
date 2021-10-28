93c95363d6642ba0bc3f39e386b6e2c20d4f8a49
Vishalcj17 <vishalcj@aospa.co>
2021-10-28 15:01:12 +0900

    bt: Build bluetooth HIDL vendor variants.
     * For backwards compatibility with VNDK.
    
    Change-Id: I4246745d0f806854854ef65ab5de5602ff11a1a2

--- system/bt/qti-bt.mk
+++ system/bt/qti-bt.mk
@@ -29,5 +29,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so
 endif
 
+# Packages
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/system/bt/bt-vendor.mk)
