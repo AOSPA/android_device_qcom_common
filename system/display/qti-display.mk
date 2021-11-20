e5607781c4be16970dc3013013b8a2fea13974a3
marcomarinho <marcomarinho12@gmail.com>
2021-11-20 23:30:14 +0900

    common: system: Update to LA.QSSI.12.0.r1-05100.02-qssi.0
    Change-Id: Ic8e141c241fc2a2572361aa474f12aac5ebeb1b3
    Co-authored-by: Vishalcj17 <vishalcj@aospa.co>
    Co-authored-by: gotenksIN <forumomkar@gmail.com>

--- system/display/qti-display.mk
+++ system/display/qti-display.mk
@@ -18,6 +18,7 @@
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 include vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk
 include vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk
+include vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk
 endif
 
 # Permissions
