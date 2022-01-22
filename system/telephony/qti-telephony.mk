0cee00b628ed5b7f1de17319c77f787aef90971e
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 00:48:24 -0700

    telephony: system: Sync up /system_ext properties with QSSI 12.
    Change-Id: Ife1c6024697038855293c419fcbd157e69998c11

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -86,7 +86,9 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.vendor.dpm.feature=11 \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=33,33 \
+    ro.telephony.sim_slots.count=2 \
     ro.vendor.use_data_netmgrd=true \
+    telephony.active_modems.max_count=2 \
     telephony.lteOnCdmaDevice=1
 
 # Force voLTE/voWIFI/viLTE
