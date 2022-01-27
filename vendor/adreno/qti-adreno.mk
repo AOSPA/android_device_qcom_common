e5bd41391571b88d40b167e2748755ff8c8f524e
Alastair Donaldson <afdx@google.com>
2022-01-27 12:35:59 +0530

    adreno: Add OpenGL ES and update Vulkan dEQP feature flags.
     * Populates a new feature flag to record the level of
       OpenGL ES dEQP test support. Updates the analogous
       Vulkan dEQP feature flag to the value for the next
       dessert release.
    
    Change-Id: I55c39601687ab66745c17598ec08e3eee12bf84c
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- vendor/adreno/qti-adreno.mk
+++ vendor/adreno/qti-adreno.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Properties
