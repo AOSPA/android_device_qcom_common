98ae621f1d0836dbb0aff6392c01d307cf120e17
Juhyung Park <qkrwngud825@gmail.com>
2022-02-13 18:04:23 +0900

    common: perf: disable zram readahead for all devices
    Change-Id: I12a4ffb916c56df30577e75098dec10694c61e66
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- vendor/init/init.qcom.post_boot.sh
+++ vendor/init/init.qcom.post_boot.sh
@@ -948,7 +948,6 @@ if [ "$ProductName" == "msmnile" ] || [ "$ProductName" == "kona" ] || [ "$Produc
       # Enable ZRAM
       configure_zram_parameters
       configure_read_ahead_kb_values
-      echo 0 > /proc/sys/vm/page-cluster
       echo 100 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
