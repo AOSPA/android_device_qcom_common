4b2862ec9db013aec9b3dd16d436018285eda194
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-03-23 15:08:48 +0900

    wfd: drop miracast libs
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I9d7335a865ae20ca4620f39e3f32a859020f27e1

--- system/wfd/qti-wfd.mk
+++ system/wfd/qti-wfd.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE_FRAMEWORK_MANIFEST_FILE += device/qcom/common/system/wfd/framework_manifest.xml
-
 PRODUCT_PACKAGES += \
     libavservices_minijail \
     libnl
