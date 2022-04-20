d083708f2a6ed6b3c6f09139e8151e56d4d35cad
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-20 13:58:27 +0900

    common: system: update to 7000 QSSI
    Change-Id: I545e124dab776816d31dd1c7236c856bdb02f7d7
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -57,6 +57,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.vendor.cne.feature=1 \
     persist.vendor.data.mode=concurrent \
     persist.vendor.dpm.feature=11 \
+    persist.vendor.dpm.idletimer.mode=default \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=33,33 \
     ro.telephony.sim_slots.count=2 \
