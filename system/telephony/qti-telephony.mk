e95dec1ff684155b0dbd3bd59f8ace76278c7158
Jake Weinstein <jake@aospa.co>
2022-06-07 18:34:19 +0000

    Move telephony framework packages out of the QTI Telephony component
    Some of these are needed to boot and use the radio
    on non-Qualcomm devices running CLO as well.
    Others such as CellBroadcastReceiver and Stk are not Qualcomm-specific
    and should be built on all devices.
    
    Change-Id: I9b6036cd727010041563bb706396a003e3a77594

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -21,33 +21,11 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(QCOM_COMMON_PATH)/system/telephony/framework
 
 # Packages
 PRODUCT_PACKAGES += \
-    CellBroadcastReceiver \
     android.hardware.radio@1.6 \
     android.hardware.radio.config@1.3 \
     android.hardware.radio.deprecated@1.0 \
     android.system.net.netd@1.1 \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    ims-ext-common \
-    ims_ext_common.xml \
-    libjson \
-    Stk \
-    tcmiface \
-    telephony-ext \
-    qti-telephony-hidl-wrapper \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper.xml \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti-telephony-utils-prd \
-    qti_telephony_utils.xml \
-    qti_telephony_utils_prd.xml
-
-PRODUCT_BOOT_JARS += \
-    tcmiface \
-    telephony-ext
+    libjson
 
 # Properties
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
