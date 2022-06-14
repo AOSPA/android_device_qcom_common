ce5b2695f470ab5252724df4c86fa54a03560355
Alexander Koskovich <akoskovich@pm.me>
2022-06-14 04:49:48 -0700

    PRODUCT_PROPERTY_OVERRIDES -> PRODUCT_VENDOR_PROPERTIES
    Change-Id: If9cfaf3b6f356fa0f880cc3ad2c2d032505f2d1e

--- vendor/adreno-legacy/qti-adreno-legacy.mk
+++ vendor/adreno-legacy/qti-adreno-legacy.mk
@@ -25,7 +25,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Properties
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
     ro.opengles.version=196610
