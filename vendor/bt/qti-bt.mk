09e9e1ad9ffcf1fa1a0dc96625eac44029ce7dcc
Jake Weinstein <jake@aospa.co>
2022-08-15 03:30:06 +0900

    bt: Build common packages
    Change-Id: I3abc190d78bc02b4ffa85eea9c8c6cfa590035a5

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -17,8 +17,15 @@ ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
 endif
 
+# Packages
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    audio.bluetooth.default \
+    com.dsi.ant@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Permissions
 PRODUCT_COPY_FILES += \
