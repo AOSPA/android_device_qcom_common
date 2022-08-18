d7c1ba1f940a7c3da5cb1d4fd5d01ea52be29a4d
Omkar Chandorkar <gotenksIN@aospa.co>
2022-08-19 00:18:11 +0900

    common: system: update to LA.QSSI.13.0.r1-05300-qssi.0
    Change-Id: I831b8368db8352861df8fc39d823d8fa252ad66f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(QCOM_COMMON_PATH)/system/telephony/framework_manifest.xml
+SYSTEM_EXT_MANIFEST_FILES += $(QCOM_COMMON_PATH)/system/telephony/atcmdfwd-saidl.xml
 
 # Packages
 PRODUCT_PACKAGES += \
