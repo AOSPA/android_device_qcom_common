0390371d98a34e7ed84210b273499797c0bd6b2c
Omkar Chandorkar <gotenksIN@aospa.co>
2022-08-20 20:15:55 +0900

    system: update to LA.QSSI.12.0.r1-08300-qssi.0
    Change-Id: I66f558ca59ef4d1ed9e1c4b26b12f468236d5225
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- components.mk
+++ components.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Current system tag :- LA.QSSI.12.0.r1-07100-qssi.0
+# Current system tag :- LA.QSSI.12.0.r1-08300-qssi.0 
 # Current vendor tag :- LA.UM.9.14.r1-19300.01-LAHAINA.QSSI12.0
 
 ifeq ($(TARGET_COMMON_QTI_COMPONENTS), all)
