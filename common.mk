7363006c48acd87688166750d4425647d994a07b
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-08-29 17:06:25 +0000

    common: Build standard vendor HIDLs
    [1] - This isn't Qualcomm specific, but currently all our devices use this in their specific device tree, so we'll move them here and clean up the device tree.
    [2] - This has become mandatory since Android 12.
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I17379162800c210bc0b3f03c9ccba8f7cd0b7c7d

--- common.mk
+++ common.mk
@@ -196,4 +196,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Common android HIDL vendor variant
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
+
 endif # QCOM_BOARD_PLATFORMS
