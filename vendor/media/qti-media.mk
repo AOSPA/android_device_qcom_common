406c187f6b71992ce51c9c14a4efb460219e0252
Eruvaram Kumar Raja Reddy <quic_erajared@quicinc.com>
2022-09-08 23:18:29 +0530

    vendor: media: bengal: Enable Codec2 for HY11 builds
    Add bengal tag to enable HY11 build.
    
    Change-Id: I4f188ac06bf9bfd3f12baeeb5f2cbeab74ed675e
    Signed-off-by: Wahid7852 <wahidzk0091@gmail.com>

--- vendor/media/qti-media.mk
+++ vendor/media/qti-media.mk
@@ -55,6 +55,17 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.audio.c2.preferred=true \
     vendor.qc2audio.suspend.enabled=true \
     vendor.qc2audio.per_frame.flac.dec.enabled=true
+
+endif
+
+#---------------------------------------------------------------------------------------------------
+# Runtime Codec2.0 enablement
+#---------------------------------------------------------------------------------------------------
+# TODO(PC): Override ccodec selection option back to defult (4).
+#           QSSI is forcing this to '1'. Must be reverted
+ifeq ($(call is-board-platform-in-list, bengal), true)
+    $(warning "Default Codec2.0 Enabled")
+    PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
 endif
 
 # Get non-open-source specific aspects.
