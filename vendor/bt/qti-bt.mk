c5f21d4537cd7af5c4ab441c452e74e1e2aa8408
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-09 23:17:49 +0000

    vendor: bt: Build libbluetooth_audio_session
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Idc746efcfca0b2bfc8f6c59813e4e46ca0d8014b

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -25,6 +25,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
