a6562764cecabd71b6e81fa974d1bce323e592bb
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-09 20:30:02 +0000

    vendor: bt: Build libldacBT packages
    [1] - It is now opensource on A13
    
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I9db9207f6e0a2047f9770645b1c7442a4d80590f

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_PACKAGES += \
+    libldacBT_enc \
+    libldacBT_abr
+
 # FM
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.fm@1.0.vendor
