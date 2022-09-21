bf9d891c7b4a6774e17345fd30a4112bb3bc2aa3
Alexander Koskovich <akoskovich@pm.me>
2022-09-21 15:56:53 -0700

    vendor: init: Update to LA.VENDOR.1.0.r1-16800-WAIPIO.QSSI13.0
    Change-Id: Ia16d51b80f402634bd918314ee3cf7691421668d

---
+++ vendor/init/anorak/bin/init.kernel.post_boot-anorak.sh
@@ -0,0 +1,6 @@
+#=============================================================================
+# Copyright (c) 2022 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#=============================================================================
+
