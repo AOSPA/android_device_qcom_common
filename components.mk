562b54d22c1ca68d5e6f2700ba4eee4916ee33d4
Alexander Koskovich <akoskovich@pm.me>
2022-10-03 20:50:37 -0700

    common: Add support for non NQ NFC platforms
     * NXP NFC is becoming increasingly popular as an alternative
       to NQ NFC, especially on the PN5xx platform. Add support
       for disabling NQ NFC in favor of NXP NFC. Rename QTI
       component to reflect this change.
    
    Change-Id: I63dd29357943d6e5a31e479e98e6ff79df9b5124

--- components.mk
+++ components.mk
@@ -25,7 +25,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     gps \
     init \
     media \
-    nq-nfc \
+    nfc \
     overlay \
     perf \
     telephony \
@@ -89,8 +89,8 @@ ifneq (,$(filter media, $(TARGET_COMMON_QTI_COMPONENTS)))
   endif
 endif
 
-ifneq (,$(filter nq-nfc, $(TARGET_COMMON_QTI_COMPONENTS)))
-  include $(QCOM_COMMON_PATH)/vendor/nq-nfc/qti-nq-nfc.mk
+ifneq (,$(filter nfc, $(TARGET_COMMON_QTI_COMPONENTS)))
+  include $(QCOM_COMMON_PATH)/vendor/nfc/qti-nfc.mk
 endif
 
 ifneq (,$(filter overlay, $(TARGET_COMMON_QTI_COMPONENTS)))
--- vendor/nfc/qti-nfc.mk
+++ vendor/nfc/qti-nfc.mk
@@ -1,22 +1,16 @@
-# Copyright (C) 2021 Paranoid Android
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2021-2022 Paranoid Android
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
+TARGET_USES_NQ_NFC ?= true
+
+ifeq ($(TARGET_USES_NQ_NFC), true)
 
 # Inherit from NQ NFC.
 $(call inherit-product, vendor/nxp/opensource/commonsys/packages/apps/Nfc/nfc_system_product.mk)
 $(call inherit-product, vendor/nxp/opensource/halimpl/nfc_vendor_product.mk)
-
-TARGET_USES_NQ_NFC := true
 
 # Packages
 PRODUCT_PACKAGES += \
@@ -36,4 +30,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(NFC_PERMISSIONS_DIR)/android.hardware.se.omapi.uicc.xml
 
 # Get non-open-source specific aspects.
-$(call inherit-product-if-exists, vendor/qcom/common/vendor/nq-nfc/nq-nfc-vendor.mk)
+$(call inherit-product-if-exists, vendor/qcom/common/vendor/nfc/nq-nfc-vendor.mk)
+
+else
+
+PRODUCT_PACKAGES += \
+    NfcNci \
+    android.hardware.nfc@1.2-service \
+    android.hardware.nfc@1.2.vendor \
+    android.hardware.secure_element@1.2-service \
+    libchrome.vendor
+
+endif
