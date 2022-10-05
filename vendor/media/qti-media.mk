efc7dcfeb76e310b0b2d98db9f09293662b2bec9
Alexander Koskovich <akoskovich@pm.me>
2022-10-05 00:28:57 -0700

    vendor: media: Add prebuilt_hidl_interfaces for dist builds
    Change-Id: I2f904f3e98af96e77c57963c85a41334dbcda840

--- vendor/media/qti-media.mk
+++ vendor/media/qti-media.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/media
+
 # Inherit configuration from the HAL.
 $(call inherit-product-if-exists, hardware/qcom/media/product.mk)
 
