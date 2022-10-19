de5fb5b1afea77e5175d747d09e68fc792f7fcbe
Jake Weinstein <jake@aospa.co>
2022-10-19 14:28:28 +0900

    common: Inherit compatibility_matrix.xml from vendor-common
    Change-Id: I7dbb6a66401ae6c3ec827a8b6037944d84ef605b

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ endif
 ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 # Compatibility matrix
+DEVICE_MATRIX_FILE += \
+    device/qcom/vendor-common/compatibility_matrix.xml
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 endif
