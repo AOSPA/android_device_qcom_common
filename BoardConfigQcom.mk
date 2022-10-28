6f4c4eb90da8c74459725cdc2d7ec46779d54cff
Jake Weinstein <jake@aospa.co>
2022-10-29 05:12:50 +0900

    common: sepolicy: Build custom policies
    Moving to device/qcom/common so we don't need
    to rebase so many changes going forward.
    
    Change-Id: I0a51c45e0bd54fe14a0bf95e7460bcd9688705ca

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -32,6 +32,7 @@ ifneq ($(call is-board-platform-in-list, msm8937 msm8953 msm8996 msm8998 sdm660)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/common/sepolicy/SEPolicy.mk
 else
 include device/qcom/sepolicy/SEPolicy.mk
 endif
---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,26 @@
+COMMON_SEPOLICY_PATH := device/qcom/common/sepolicy
+
+ifeq ($(TARGET_SEPOLICY_DIR),)
+    TARGET_SEPOLICY_DIR := $(TARGET_BOARD_PLATFORM)
+endif
+
+ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+    BOARD_VENDOR_SEPOLICY_DIRS += \
+        $(COMMON_SEPOLICY_PATH)/generic/vendor/common \
+        $(COMMON_SEPOLICY_PATH)/qva/vendor/common \
+        $(COMMON_SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR) \
+        $(COMMON_SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
+else # 845 and 710
+    BOARD_VENDOR_SEPOLICY_DIRS += \
+        $(COMMON_SEPOLICY_PATH)/legacy/vendor/ssg \
+        $(COMMON_SEPOLICY_PATH)/legacy/vendor/common \
+        $(COMMON_SEPOLICY_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+        BOARD_VENDOR_SEPOLICY_DIRS += \
+            $(COMMON_SEPOLICY_PATH)/legacy/vendor/test \
+            $(COMMON_SEPOLICY_PATH)/legacy/vendor/test/sysmonapp
+    endif
+
+endif # 845 and 710
+
