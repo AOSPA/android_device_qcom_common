4561410e9cf062b126bd0b8888ee333b80b07e32
Jake Weinstein <jake@aospa.co>
2022-11-07 03:51:56 +0900

    vendor: init: Allow 5.4+ devices to boot without modules
    init.qti.kernel.rc waits for vendor.all.modules.ready to be
    set before finishing init. This is never set when there
    are no modules present. Adding vendor_modprobe_no_modules.sh
    to unconditionally set vendor.all.modules.ready and boot.
    
    Change-Id: I59f9ae8408f1341469ab7dd02c2ad1222f711fae

---
+++ vendor/init/vendor_modprobe_no_modules.sh
@@ -0,0 +1,9 @@
+#! /vendor/bin/sh
+#=============================================================================
+# Copyright (c) 2019-2021 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#=============================================================================
+
+setprop vendor.all.modules.ready 1
+exit 0
