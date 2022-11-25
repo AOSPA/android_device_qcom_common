a8874753a026095dc4dc91f3b120e9d46a7cae27
Tobias Merkel <stylogey@gmail.com>
2022-11-25 20:11:20 +0100

    Revert "common: skip sepolicy inclusions for legacy socs"
    This reverts commit 6f4c8c93b845c8340de908826ae9edc34a139f2a.
    
    Change-Id: I8ef53d40297d1ae038a23be7f2505fad3877ad6a

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -28,7 +28,6 @@ endif
 TARGET_MOUNT_POINTS_SYMLINKS ?= false
 
 # SEPolicy
-ifneq ($(call is-board-platform-in-list, msm8937 msm8953 msm8996 msm8998 sdm660),true)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifneq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
@@ -37,6 +36,3 @@ else
 include device/qcom/sepolicy/SEPolicy.mk
 endif
 endif # Exclude QCOM SEPolicy
-else
-include device/qcom/sepolicy-legacy/SEPolicy.mk
-endif
