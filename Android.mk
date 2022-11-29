7e1f5ee252d1551bd1d154ccea66584d71b73a22
Jake Weinstein <jake@aospa.co>
2022-11-30 08:09:37 +0900

    common: Guard AndroidBoardCommon.mk
    Without this change, AndroidBoardCommon.mk
    is inherited on all devices even if they
    are non-QCOM.
    
    This fixes the following errors:
    [  0% 768/138981] Creating /out/jake/target/product/oriole/vendor/bt_firmware
    FAILED: /out/jake/target/product/oriole/vendor/bt_firmware
    /bin/bash -c "(mkdir -p /out/jake/target/product/oriole/vendor/bt_firmware ) && (ln -sf /vendor/bt_firmware /out/jake/target/product/oriole/root/bt_firmware )"
    ln: cannot create symbolic link from '/vendor/bt_firmware' to '/out/jake/target/product/oriole/root/bt_firmware': No such file or directory
    [  0% 769/138981] Creating /out/jake/target/product/oriole/vendor/dsp
    FAILED: /out/jake/target/product/oriole/vendor/dsp
    /bin/bash -c "(mkdir -p /out/jake/target/product/oriole/vendor/dsp ) && (ln -sf /vendor/dsp /out/jake/target/product/oriole/root/dsp )"
    ln: cannot create symbolic link from '/vendor/dsp' to '/out/jake/target/product/oriole/root/dsp': No such file or directory
    
    Change-Id: Id8938d36ae70c5bd3a4462bc875e390e8228cd62

--- Android.mk
+++ Android.mk
@@ -17,6 +17,8 @@ LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 # Common
+ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
 -include vendor/qcom/opensource/core-utils/build/AndroidBoardCommon.mk
+endif
 endif
