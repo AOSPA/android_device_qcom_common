38f1c28c20355401279abef419f83804a342f643
Jake Weinstein <jake@aospa.co>
2022-11-29 07:27:30 +0000

    vendor: adreno: Conditionally upgrade to LA.VENDOR.1.0.r1-16000-WAIPIO.QSSI13.0
    Currently, this only works on 5.4 and 5.10,
    so pre-5.4 devices will stay on 5.4
    binaries for the time being. This
    renames components to the below:
    
    adreno: 5.4/5.10 (LA.VENDOR.1.0.r1-16000-WAIPIO.QSSI13.0)
    adreno-6xx-legacy: 4.9-4.19 (LA.UM.9.14.r1-20200-LAHAINA.QSSI13.0)
    adreno-5xx: 3.18/4.4 (LA.UM.10.6.2.r1-01600-89xx.0)
    
    Change-Id: I892e3b2e3c5c1d6d95d7c0ab4674075aa5546a3a

--- vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk
+++ vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk
@@ -12,16 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Dependencies
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.display.mapper@2.0.vendor
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Properties
@@ -31,4 +28,4 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.opengles.version=196610
 
 # Get non-open-source specific aspects.
-$(call inherit-product, vendor/qcom/common/vendor/adreno-legacy/adreno-legacy-vendor.mk)
+$(call inherit-product, vendor/qcom/common/vendor/adreno-6xx-legacy/adreno-6xx-legacy-vendor.mk)
