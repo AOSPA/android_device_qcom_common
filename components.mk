d2209ba8d9d6a8f7ec83d478b944383886f223f2
Jake Weinstein <jake@aospa.co>
2022-12-01 07:56:02 +0900

    components: Enable media component to bengal
    [1] - Everything being used here is also cane enabled on SM8450 audio hal.
    
    Change-Id: Iee0cb5cd7dd872f72b5b9ebe8244f9910146c990
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- components.mk
+++ components.mk
@@ -80,7 +80,7 @@ ifneq (,$(filter init, $(TARGET_COMMON_QTI_COMPONENTS)))
 endif
 
 ifneq (,$(filter media, $(TARGET_COMMON_QTI_COMPONENTS)))
-  ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY)),true)
+  ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) bengal),true)
     include $(QCOM_COMMON_PATH)/vendor/media/qti-media.mk
   else ifeq ($(call is-board-platform-in-list,$(5_4_FAMILY)),true)
     include $(QCOM_COMMON_PATH)/vendor/media-5.4/qti-media-5.4.mk
