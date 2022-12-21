3f6106f53e2880c054d83e56ec7d66fc6cec255c
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-22 04:19:40 +0530

    vendor: init: Directly set modules prop instead of script
    For some reason, even when KERNEL_MODULES_OUT is set
    it's using the no_modules script. Avoid this and set
    the modules.ready prop directly if there are no modules.
    
    This partially reverts commit 93382fa26a1eab9dcfa2a2c4c3d4771901fb9c58.
    
    Change-Id: I237c6b31f335a71913d24a9c15f9c120ac24b1ba

--- vendor/init/vendor_modprobe_no_modules.sh
+++
@@ -1,9 +0,0 @@
-#! /vendor/bin/sh
-#=============================================================================
-# Copyright (c) 2019-2021 Qualcomm Technologies, Inc.
-# All Rights Reserved.
-# Confidential and Proprietary - Qualcomm Technologies, Inc.
-#=============================================================================
-
-setprop vendor.all.modules.ready 1
-exit 0
