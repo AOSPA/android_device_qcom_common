ad35d0dacaab8e1ea29f94333359adbe657802ea
Jake Weinstein <jake@aospa.co>
2023-01-06 04:32:06 +0000

    common: sepolicy: legacy: Import changes from LA.UM.9.6.4.r1-03900-89xx.QSSI13.0
    Includes support for MSM8937/MSM8953/MSM8998/SDM660
    
    Change-Id: Iaa111b2eebaf7ef755b57cea26d6c4ba0a4d5def

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -4,7 +4,7 @@ ifeq ($(TARGET_SEPOLICY_DIR),)
     TARGET_SEPOLICY_DIR := $(TARGET_BOARD_PLATFORM)
 endif
 
-ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter sdm845 sdm710 sdm660 msm8937 msm8953 msm8996, $(TARGET_BOARD_PLATFORM)))
     SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
         $(COMMON_SEPOLICY_PATH)/generic/private/common
     BOARD_VENDOR_SEPOLICY_DIRS += \
@@ -12,7 +12,7 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
         $(COMMON_SEPOLICY_PATH)/qva/vendor/common \
         $(COMMON_SEPOLICY_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR) \
         $(COMMON_SEPOLICY_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
-else # 845 and 710
+else # Legacy
     BOARD_VENDOR_SEPOLICY_DIRS += \
         $(COMMON_SEPOLICY_PATH)/legacy/vendor/ssg \
         $(COMMON_SEPOLICY_PATH)/legacy/vendor/common \
@@ -24,4 +24,4 @@ else # 845 and 710
             $(COMMON_SEPOLICY_PATH)/legacy/vendor/test/sysmonapp
     endif
 
-endif # 845 and 710
+endif
