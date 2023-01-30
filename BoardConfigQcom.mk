167d7b10a96e4d40120d90a3fed6734ebdc6e66f
enesykaya <enesykaya@gmail.com>
2023-01-30 23:30:28 +0300

    common: sepolicy: Add sdm710 & sdm845 into legacy sepolicy
    Change-Id: I30974a87474d906917685e9f47a2d80deb4e4596

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -29,10 +29,10 @@ TARGET_MOUNT_POINTS_SYMLINKS ?= false
 
 # SEPolicy
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
-ifneq ($(call is-board-platform-in-list, msm8937 msm8953 msm8998 sdm660),true)
+ifneq ($(call is-board-platform-in-list, msm8937 msm8953 msm8998 sdm660 sdm710 sdm845),true)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-else # if (8937 || 8953 || 8998 || 660)
+else # if (8937 || 8953 || 8998 || 660 || 710 || 845)
 include device/qcom/sepolicy/SEPolicy.mk
-endif # !(8937 || 8953 || 8998 || 660)
+endif # !(8937 || 8953 || 8998 || 660 || 710 || 845)
 include device/qcom/common/sepolicy/SEPolicy.mk
 endif # Exclude QCOM SEPolicy
