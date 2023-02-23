45f4649b88b77eb4a318c18303a3840e64afbcf6
electimon <electimon@gmail.com>
2023-02-23 08:53:49 +0800

    common: vendor: Introduce dsprpcd module
    * Based upon LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0,
    * This module contains *dsprpcd services with
    * a selector to enable the correct services
    
    TODO: Verify selectors for all SoCs
    
    Change-Id: I79aaa8fd359ba4c8450f5896b9cfb873cec1f468
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendor/dsprpcd/qti-dsprpcd.mk
@@ -0,0 +1,27 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq ($(KERNEL_VERSION),4.14)
+PRODUCT_COPY_FILES += $(QCOM_COMMON_PATH)/vendor/dsprpcd/vendor.qti.adsprpc-sscrpc-service.rc:vendor/etc/init/vendor.qti.adsprpc-sscrpc-service.rc
+else
+ifneq ($(TARGET_BOARD_PLATFORM),sdm660)
+PRODUCT_COPY_FILES += $(QCOM_COMMON_PATH)/vendor/dsprpcd/vendor.qti.adsprpc-guestos-service.rc:vendor/etc/init/vendor.qti.adsprpc-guestos-service.rc
+PRODUCT_COPY_FILES += $(QCOM_COMMON_PATH)/vendor/dsprpcd/vendor.sensors.sscrpcd.rc:vendor/etc/init/vendor.sensors.sscrpcd.rc
+else
+PRODUCT_COPY_FILES += $(QCOM_COMMON_PATH)/vendor/dsprpcd/vendor.qti.adsprpc-guestos-noaudiopd-service.rc:vendor/etc/init/vendor.qti.adsprpc-guestos-noaudiopd-service.rc
+endif
+endif
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/vendor/dsprpcd/dsprpcd-vendor.mk)
