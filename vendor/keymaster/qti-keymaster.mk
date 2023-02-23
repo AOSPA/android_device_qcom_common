7b86f9687d402e3b9f974c3c73640710dbc91115
electimon <electimon@gmail.com>
2023-02-23 09:17:44 +0800

    common: vendor: Introduce keymaster module
    * Based upon LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0,
    * keymaster module provides keymaster/gatekeeper blobs
    * specifically version 4.1 which is compatible with devices
    * that use 4.0.
    
    TODO: Test this stack on 5.10 targets
    Change-Id: I216b047ce7c3bcab5ba25eeb8c359c0d4cee5865
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendor/keymaster/qti-keymaster.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Packages
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.keymaster@4.1.vendor
+
+DEVICE_MANIFEST_FILE += $(QCOM_COMMON_PATH)/vendor/keymaster/keymaster-manifest.xml
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/vendor/keymaster/keymaster-vendor.mk)
