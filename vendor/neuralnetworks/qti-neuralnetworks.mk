9c38065e305884212acffb17d1346f38131ed2c3
electimon <electimon@gmail.com>
2023-02-23 09:50:43 +0800

    common: vendor: Introduce neuralnetworks module
    * Based upon LA.UM.9.12.r1-15100-SMxx50.QSSI13.0,
    * this commit introduces a common neuralnetworks
    * stack
    
    Change-Id: I9f46acf3796cc72ca20d5fd091f29461107e963f
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendor/neuralnetworks/qti-neuralnetworks.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2021 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_MANIFEST_FILE += $(QCOM_COMMON_PATH)/vendor/neuralnetworks/android.hardware.neuralnetworks@1.3-service-qti.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0.vendor \
+    android.hardware.neuralnetworks@1.1.vendor \
+    android.hardware.neuralnetworks@1.2.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
+
+# Get non-open-source specific aspects.
+$(call inherit-product, vendor/qcom/common/vendor/neuralnetworks/neuralnetworks-vendor.mk)
