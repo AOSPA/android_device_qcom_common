c0ea0dc4c20b4701e8a162af65e0d4513c0cb895
electimon <electimon@gmail.com>
2023-02-26 20:07:18 +0800

    common: system/audio: Use optional includes
    * The audio component provides important configuration,
      we should allow it to be used even if the audio HAL
      isn't at the hardcoded location. This changes also
      matches vendor/audio's optional includes.
    
    Change-Id: Ibbd8797b5352f7889ede0ec618cb40af39ad80cd
    Signed-off-by: electimon <electimon@gmail.com>

--- system/audio/qti-audio.mk
+++ system/audio/qti-audio.mk
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 # Inherit from QSSI audio makefiles.
-include $(TOPDIR)vendor/qcom/opensource/commonsys/audio/configs/qssi/qssi.mk
-include $(TOPDIR)vendor/qcom/opensource/commonsys/audio/configs/qssi/audio_system_product.mk
+-include $(TOPDIR)vendor/qcom/opensource/commonsys/audio/configs/qssi/qssi.mk
+-include $(TOPDIR)vendor/qcom/opensource/commonsys/audio/configs/qssi/audio_system_product.mk
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/system/audio/audio-vendor.mk)
