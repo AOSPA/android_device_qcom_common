780cbb760cfc109afd8423121a93b6a591ffa2c8
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-02-27 09:25:58 -0800

    vendor: media/media-legacy: Add vppservice_interface prebuilt_hidl_interfaces
    [1] - https://gerrit.aospa.co/c/AOSPA/android_device_qcom_common/+/28005
    
    Change-Id: Ia433be924f34352aebe7d40fed5e95f76999a445
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/media-legacy/qti-media-legacy.mk
+++ vendor/media-legacy/qti-media-legacy.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/media-legacy
+
 TARGET_MEDIA_COMPONENT_VARIANT := media-legacy
 
 # Inherit configuration from the HAL.
