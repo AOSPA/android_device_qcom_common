5f5a51209556de418bbe91891969bc91c2780d71
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-03-14 22:29:55 +0530

    vendor: Move perf init script to init component
    Makes no sense to duplicate it across both perf components.
    
    Change-Id: I954fc77155ad5a6a1ea421b57d26c2f0f4514b69

--- vendor/perf/qti-perf.mk
+++ vendor/perf/qti-perf.mk
@@ -40,7 +40,6 @@ endif
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
-    init.aospa.perf.rc \
     libavservices_minijail.vendor \
     libpsi.vendor \
     libtflite \
