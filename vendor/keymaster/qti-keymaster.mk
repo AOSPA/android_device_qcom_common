9e591338e22502753954ba6a4a33f7c0501f01f7
electimon <electimon@gmail.com>
2023-03-14 15:21:55 +0800

    common: vendor/keymaster: Include gatekeeper vintf fragment
    Change-Id: I55c50119e26989ff1fbed2df7b6798b20fe9cd4f
    Signed-off-by: electimon <electimon@gmail.com>

--- vendor/keymaster/qti-keymaster.mk
+++ vendor/keymaster/qti-keymaster.mk
@@ -18,7 +18,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
     android.hardware.keymaster@4.1.vendor
 
-DEVICE_MANIFEST_FILE += $(QCOM_COMMON_PATH)/vendor/keymaster/keymaster-manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(QCOM_COMMON_PATH)/vendor/keymaster/gatekeeper-manifest.xml \
+    $(QCOM_COMMON_PATH)/vendor/keymaster/keymaster-manifest.xml
 
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/keymaster/keymaster-vendor.mk)
