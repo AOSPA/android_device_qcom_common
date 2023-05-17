74f040f1520ad6c692f53fc024670578a1b537c4
Yuan Si <do4suki@gmail.com>
2023-05-17 09:13:10 +0800

    media-legacy: Replace LOCAL_COPY_HEADERS with headers lib
    Change-Id: I4aa16a6aeabc878ae240776e823dff9a776032b3
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- vendor/media-legacy/Android.mk
+++ vendor/media-legacy/Android.mk
@@ -2,12 +2,9 @@ ifeq ($(TARGET_MEDIA_COMPONENT_VARIANT),media-legacy)
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BUILD_BROKEN_USES_BUILD_COPY_HEADERS),true)
 include $(CLEAR_VARS)
-LOCAL_COPY_HEADERS_TO := fastcv
-LOCAL_COPY_HEADERS := include/fastcv.h
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_COPY_HEADERS)
-endif
+LOCAL_MODULE := fastcv_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+include $(BUILD_HEADER_LIBRARY)
 
 endif
