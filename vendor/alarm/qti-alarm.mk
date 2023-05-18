ed1167bf4139cfa9aa9a26aec6e29de9dd26e342
electimon <electimon@gmail.com>
2023-05-17 23:09:00 -0400

    common: Introduce alarm component
    * Based upon LA.UM.9.14.r1-21000-LAHAINA.QSSI12.0
                 LA.QSSI.13.0.r1-09700-qssi.0
    
    Change-Id: I7085e5686c6cc2d63a62f84557bc1bac3d579e67
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ vendor/alarm/qti-alarm.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/vendor/alarm/alarm-vendor.mk)
