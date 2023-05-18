397987d59cd0b3a5bfed662aa5b0ee3eba6d72fd
electimon <electimon@gmail.com>
2023-05-17 23:09:00 -0400

    common: Introduce alarm component
    * Based upon LA.UM.9.14.r1-21000-LAHAINA.QSSI12.0
                 LA.QSSI.13.0.r1-09700-qssi.0
    
    Change-Id: I7085e5686c6cc2d63a62f84557bc1bac3d579e67
    Signed-off-by: electimon <electimon@gmail.com>

--- components.mk
+++ components.mk
@@ -15,6 +15,7 @@
 ifneq (,$(filter all, $(TARGET_COMMON_QTI_COMPONENTS)))
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
+    alarm \
     audio \
     av \
     bt \
@@ -43,6 +44,11 @@ ifneq (,$(filter adreno, $(TARGET_COMMON_QTI_COMPONENTS)))
   else
     include $(QCOM_COMMON_PATH)/vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk
   endif
+endif
+
+ifneq (,$(filter alarm, $(TARGET_COMMON_QTI_COMPONENTS)))
+  include $(QCOM_COMMON_PATH)/system/alarm/qti-alarm.mk
+  include $(QCOM_COMMON_PATH)/vendor/alarm/qti-alarm.mk
 endif
 
 ifneq (,$(filter audio, $(TARGET_COMMON_QTI_COMPONENTS)))
