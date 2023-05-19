ce0e905abe528c3ca91425d6f1806e535c2aa903
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-05-19 17:44:15 +0000

    vendor: Introduce gps-legacy component and vendor proprietary
    [1] - Qualcomm is using the exact same hal and GPS blobs for all SoC with A11 GRF, so we will define this as Common
    [2] - Use HAL and Blobs from LA.UM.9.15.2.r1 because it updates faster than other platforms
    [3] - HAL and blobs from LA.UM.9.15.2.r1-06600-KAMORTA.QSSI-13.0
    
    Change-Id: Ia4c40020ff9155f178fd52e307ef278806f40046
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

---
+++ vendor/gps-legacy/qti-gps-legacy.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2022 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/gps
+
+# Flags
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+
+# Inherit the GPS HAL.
+$(call inherit-product-if-exists, hardware/qcom/gps/gps_vendor_product.mk)
+
+# Overlays
+PRODUCT_PACKAGES += \
+    QCOMGPSFrameworksOverlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/vendor/gps-legacy/gps-legacy-vendor.mk)
