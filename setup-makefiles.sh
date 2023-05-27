aaae7198d642119c3f587a58034ca29f0c04e7b5
electimon <electimon@gmail.com>
2023-05-27 21:20:48 +0000

    common: Consolidate extract utils scripts
    * Instead of numerous separate standalone scripts,
      we can have just 2 symlinked to where we need them.
    
    test: extract system/av, vendor/dsprpcd, vendor/display/5.4
    Change-Id: Ib6a42c76c1141f14855f7d06188b62008f28e20d
    Signed-off-by: electimon <electimon@gmail.com>

---
+++ setup-makefiles.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2020 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+SCRIPT_LOCATION=$(readlink -f "${BASH_SOURCE[0]}")
+ANDROID_ROOT=$(dirname $SCRIPT_LOCATION)"/../../.."
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Get the COMPONENT, KERNEL_VERSION, and VENDOR from the current directory
+CURRENT_DIR="$(pwd)"
+
+if [[ "$(basename $(realpath $CURRENT_DIR/../..))" == "system" ]] || \
+   [[ "$(basename $(realpath $CURRENT_DIR/..))" == "system" ]] ; then
+    VENDOR="qcom/common/system"
+elif [[ "$(basename $(realpath $CURRENT_DIR/../..))" == "vendor" ]] || \
+   [[ "$(basename $(realpath $CURRENT_DIR/..))" == "vendor" ]] ; then
+    VENDOR="qcom/common/vendor"
+fi
+
+# Extract the kernel version if it follows the format x.y
+if [[ "${CURRENT_DIR}" =~ /([0-9]+\.[0-9]+)$ ]]; then
+    KERNEL_VERSION="${BASH_REMATCH[1]}"
+fi
+
+if [ ! -z "${KERNEL_VERSION}" ]; then
+    COMPONENT=$(basename $(realpath "$CURRENT_DIR/.."))
+else
+    COMPONENT=$(basename "$CURRENT_DIR")
+fi
+
+# Initialize the helper
+if [ ! -z ${KERNEL_VERSION} ]; then
+    setup_vendor "${COMPONENT}/${KERNEL_VERSION}" "${VENDOR}" "${ANDROID_ROOT}" false true "${COMPONENT}" true
+else
+    setup_vendor "${COMPONENT}" "${VENDOR}" "${ANDROID_ROOT}" false true "" true
+fi
+
+# Warning headers and guards
+write_headers
+
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
