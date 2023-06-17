1b013a6053530a2aad490d7b5c73c8f100e460fe
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-16 23:45:07 -0400

    vendor: wlan/wlan-legacy: Remove soong condition to IPACM
    [1] - We already have the component conditions for this
    
    Change-Id: I10e0a72c0d9b32cfeab709d98b94125a7fade286
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/wlan-legacy/qti-wlan-legacy.mk
+++ vendor/wlan-legacy/qti-wlan-legacy.mk
@@ -68,13 +68,8 @@ WLAN_KBUILD_OPTIONS_wlan := CONFIG_CNSS_SM6150=y
 endif
 
 # IPACM
-ifneq (,$(filter 5.10, $(TARGET_KERNEL_VERSION)))
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
-$(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk)
-else
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr-legacy
 $(call inherit-product, vendor/qcom/opensource/data-ipa-cfg-mgr-legacy/ipacm_vendor_product.mk)
-endif
 
 # Include QCOM WLAN makefiles.
 ifeq ($(call is-board-platform-in-list,sdm845),true)
