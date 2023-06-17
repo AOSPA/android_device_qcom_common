94b516196abbcfcb920ec9c2626722dde3c854c5
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-16 23:13:46 -0400

    common: Create condition to check only kernel version
    [1] - This will arrive if conditions will be non-legacy for bengal and Kona 5.15 kernel
    
    Change-Id: I3227cbf121f224b8673ac2fe3ddd98e68cbaf9fc
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- components.mk
+++ components.mk
@@ -38,6 +38,7 @@ endif
 
 ifneq (,$(filter adreno, $(TARGET_COMMON_QTI_COMPONENTS)))
   ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) $(5_15_FAMILY)),true)
+  else ifeq ($(TARGET_KERNEL_VERSION),5.15)
     include $(QCOM_COMMON_PATH)/vendor/adreno/qti-adreno.mk
   else ifeq ($(call is-board-platform-in-list,$(3_18_FAMILY) $(4_4_FAMILY) msm8953),true)
     include $(QCOM_COMMON_PATH)/vendor/adreno-5xx/qti-adreno-5xx.mk
@@ -80,6 +81,7 @@ endif
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) $(5_15_FAMILY)),true)
+else ifeq ($(TARGET_KERNEL_VERSION),5.15)
   include $(QCOM_COMMON_PATH)/dlkm/qti-dlkm.mk
 endif
 endif
@@ -99,6 +101,7 @@ endif
 
 ifneq (,$(filter media, $(TARGET_COMMON_QTI_COMPONENTS)))
   ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) $(5_15_FAMILY)),true)
+  else ifeq ($(TARGET_KERNEL_VERSION),5.15)
     include $(QCOM_COMMON_PATH)/vendor/media/qti-media.mk
   else ifeq ($(call is-board-platform-in-list,$(5_4_FAMILY)),true)
     include $(QCOM_COMMON_PATH)/vendor/media-5.4/qti-media-5.4.mk
@@ -118,6 +121,7 @@ endif
 ifneq (,$(filter perf, $(TARGET_COMMON_QTI_COMPONENTS)))
   include $(QCOM_COMMON_PATH)/system/perf/qti-perf.mk
   ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) $(5_15_FAMILY)),true)
+  else ifeq ($(TARGET_KERNEL_VERSION),5.15)
     include $(QCOM_COMMON_PATH)/vendor/perf/qti-perf.mk
   else
     include $(QCOM_COMMON_PATH)/vendor/perf-legacy/qti-perf-legacy.mk
@@ -126,6 +130,7 @@ endif
 
 ifneq (,$(filter qseecomd, $(TARGET_COMMON_QTI_COMPONENTS)))
   ifeq ($(call is-board-platform-in-list,$(5_4_FAMILY) $(5_10_FAMILY) $(5_15_FAMILY)),true)
+  else ifeq ($(TARGET_KERNEL_VERSION),5.15)
     include $(QCOM_COMMON_PATH)/vendor/qseecomd/qti-qseecomd.mk
   else
     include $(QCOM_COMMON_PATH)/vendor/qseecomd-legacy/qti-qseecomd-legacy.mk
@@ -151,6 +156,7 @@ endif
 
 ifneq (,$(filter wlan, $(TARGET_COMMON_QTI_COMPONENTS)))
   ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY) $(5_15_FAMILY)),true)
+  else ifeq ($(TARGET_KERNEL_VERSION),5.15)
     include $(QCOM_COMMON_PATH)/vendor/wlan/qti-wlan.mk
   else
     include $(QCOM_COMMON_PATH)/vendor/wlan-legacy/qti-wlan-legacy.mk
