a2827be632f99bd8d691285e0077deb35aef1cf3
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-17 00:06:50 -0400

    vendor: perf/perf-legacy: Clear all unnecessary conditions and stuff
    [1] - Currently only 5.10 and 5.15 support perf 2.3 Hal, so do a cleanup based on that. If one day some other legacy platform uses 2.3 perf Hal, we will bring the configs back
    
    Change-Id: I4300285e59c14ed41a52107edbbfb5594773c90e
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/perf/qti-perf.mk
+++ vendor/perf/qti-perf.mk
@@ -23,18 +23,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(QCOM_COMMON_PATH)/vendor/perf/configs/$(TARGET_BOARD_PLATFORM),$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,$(QCOM_COMMON_PATH)/vendor/perf/configs/test,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Disable IOP HAL for select platforms.
-ifeq ($(call is-board-platform-in-list, msm8937 msm8953 msm8998 qcs605 sdm660 sdm710),true)
-PRODUCT_COPY_FILES += \
-    $(QCOM_COMMON_PATH)/vendor/perf/vendor.qti.hardware.iop@2.0-service-disable.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.iop@2.0-service-disable.rc
-endif
-
-# Disable the poweropt service for <5.4 platforms.
-ifeq (,$(filter 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
-PRODUCT_COPY_FILES += \
-    $(QCOM_COMMON_PATH)/vendor/perf/poweropt-service-disable.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/poweropt-service-disable.rc
-endif
-
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
@@ -42,12 +30,6 @@ PRODUCT_PACKAGES += \
     libpsi.vendor \
     libtflite \
     vendor.qti.hardware.servicetracker@1.2.vendor
-
-# Only copy task_profiles.json for 5.4 targets.
-ifeq ($(TARGET_KERNEL_VERSION),5.4)
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-endif
 
 # Properties
 PRODUCT_VENDOR_PROPERTIES += \
@@ -62,21 +44,13 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.perf.framepacing.enable=1
 endif
 
-ifeq ($(call is-board-platform-in-list, kalama kona lahaina parrot taro),true)
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.beluga.p=0x3 \
     ro.vendor.beluga.c=0x4800 \
     ro.vendor.beluga.s=0x900 \
     ro.vendor.beluga.t=0x240
-endif
 
-ifneq (,$(filter 4.14 4.19 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
-ifeq ($(TARGET_BOARD_PLATFORM), holi)
-PRODUCT_VENDOR_PROPERTIES += vendor.pasr.activemode.enabled=false
-else
 PRODUCT_VENDOR_PROPERTIES += vendor.pasr.activemode.enabled=true
-endif
-endif
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/perf/perf-vendor.mk)
