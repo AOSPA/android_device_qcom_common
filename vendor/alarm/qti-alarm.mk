9e90b95e0ceb1382fb982e529d5579646f95961b
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-21 22:23:26 -0400

    vendor: alarm: Fix power_off_alarm on offline charging
    [1] - Qualcomm doesn't enable this by default on some platforms, so some OEMs like Xiaomi and OnePlus add this to fix it.
    
    Change-Id: I7c786e090d96d6c5b71eaa4a18222434167a59dc
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/alarm/qti-alarm.mk
+++ vendor/alarm/qti-alarm.mk
@@ -4,5 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/alarm
+
+# Init script
+PRODUCT_PACKAGES += \
+    init.qcom.alarm.rc
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/alarm/alarm-vendor.mk)
