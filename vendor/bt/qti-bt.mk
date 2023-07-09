cd110b7c08179624b12d1a1f855418d2ba04702d
CHRISL7 <wandersonrodriguesf1@gmail.com>
2023-07-09 09:55:40 -0400

    [WIP] vendor: bt: Define common plataform bt properties
    [1] - From LA.UM.9.15.2.r1-07000-KAMORTA.QSSI13.0 and LA.VENDOR.13.2.1.r1-06400-DIVAR.0
    
    Change-Id: I4ded4266669bfa02863a1dc17c0c49ef64fc9cdd
    Signed-off-by: CHRISL7 <wandersonrodriguesf1@gmail.com>

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -39,5 +39,331 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+#ANT/BT/FM/WIPOWER PROPERTIES
+
+ifeq ($(TARGET_BOARD_PLATFORM), msmnile) # msmnile specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), sdm845) # SDM845 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+
+ifeq ($(TARGET_BOARD_PLATFORM), sm6150)  # Talos/sm6150 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), sdm710)  # sdm710/Warlock specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), kona)  # kona/sm8250 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=hastings
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#A2dp Multicast support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_mcast_test.enabled=false
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true
+#AptX Adaptive R2.1 support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=false
+#HearingAid support
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fflag.override.settings_bluetooth_hearing_aid=true
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), lahaina) # lahaina specific defines
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#A2dp Multicast support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_mcast_test.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true
+#AptX Adaptive R2.1 support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=true
+#HearingAid support
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fflag.override.settings_bluetooth_hearing_aid=true
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), trinket) # trinket specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), lito) # lito specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true
+#AptX Adaptive R2.1 support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=false
+#HearingAid support
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fflag.override.settings_bluetooth_hearing_aid=true
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), bengal) # bengal specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), holi) # holi specific defines
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), atoll) # atoll specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), sdm660)  # sdm660
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8998) # MSM8998 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=cherokee
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=true
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=true
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8996) # MSM8996 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=rome
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=false
+# Set this true as ROME which is programmed
+# as embedded wipower mode by default
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8937) # msm8937 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=pronto
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=false
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8953) # MSM8953 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=pronto
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=false
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8909) # MSM8909 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=pronto
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=false
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), msm8952) # MSM8952 specific defines
+#Bluetooth SOC type
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.soc=pronto
+#split a2dp support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.enable.splita2dp=false
+#Embedded wipower mode
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.wipower=false
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), taro) # waipio specific defines
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#A2dp Multicast support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_mcast_test.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true
+#AptX Adaptive R2.1 support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=true
+#HearingAid support
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fflag.override.settings_bluetooth_hearing_aid=true
+#QC CSIP enable
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.csip_qti=true
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM), parrot) # Netrani specific defines
+#a2dp offload capability
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2
+#aac frame control support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_frm_ctl.enabled=true
+#TWS+ state support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.twsp_state.enabled=false
+#A2dp Multicast support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.a2dp_mcast_test.enabled=false
+#Scrambling support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.scram.enabled=false
+#AAC VBR support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true
+#AptX Adaptive R2.1 support
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=true
+#HearingAid support
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fflag.override.settings_bluetooth_hearing_aid=true
+#QC CSIP enable
+PRODUCT_PROPERTY_OVERRIDES += ro.vendor.bluetooth.csip_qti=true
+endif
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/bt/bt-vendor.mk)
