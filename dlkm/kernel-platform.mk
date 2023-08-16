f9dd551db60328ee68bf2ad8bb715f1c4c74a856
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-08-16 12:21:11 -0400

    common: dlkm: Add support to system_dlkm kernel modules build
    [1] - From LA.VENDOR.13.2.1.r1-06400-DIVAR.0
    
    Change-Id: I73700239b00a496f753cdee69c11d8237f6e3a85
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- dlkm/kernel-platform.mk
+++ dlkm/kernel-platform.mk
@@ -17,6 +17,9 @@ ifeq ($(wildcard $(KERNEL_PREBUILT_DIR)/),)
 $(warning $(KERNEL_PREBUILT_DIR) does not exist, have you compiled kernel?)
 endif
 
+# Path to system_dlkm artifacts directory
+BOARD_SYSTEM_DLKM_SRC := $(KERNEL_PREBUILT_DIR)/system_dlkm
+
 # DLKM
 define get-kernel-modules
 $(if $(wildcard $(KERNEL_PREBUILT_DIR)/$(1)/$(2)), \
@@ -27,17 +30,25 @@ endef
 prepend-kernel-modules = $(eval $1 := $2 $(filter-out $2,$($1)))
 
 first_stage_modules := $(call get-kernel-modules,vendor_ramdisk,modules.load)
+gki_modules := $(call get-kernel-modules,system_dlkm,modules.load)
 second_stage_modules := $(call get-kernel-modules,vendor_dlkm,modules.load)
 recovery_modules := $(call get-kernel-modules,vendor_ramdisk,modules.load.recovery)
 
 $(call prepend-kernel-modules,BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD,$(first_stage_modules))
 $(call prepend-kernel-modules,BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD,$(recovery_modules))
+$(call prepend-kernel-modules,BOARD_GKI_KERNEL_MODULES,$(gki_modules))
 $(call prepend-kernel-modules,BOARD_VENDOR_KERNEL_MODULES,$(second_stage_modules))
 
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(wildcard $(KERNEL_PREBUILT_DIR)/vendor_dlkm/modules.blocklist)
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(wildcard $(KERNEL_PREBUILT_DIR)/vendor_dlkm/modules.blocklist)
 
+ifneq ("$(wildcard $(KERNEL_PREBUILT_DIR)//vendor_dlkm/system_dlkm.modules.blocklist)", "")
+PRODUCT_COPY_FILES += $(KERNEL_PREBUILT_DIR)//vendor_dlkm/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+endif
+
+
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(first_stage_modules)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(gki_modules)
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(second_stage_modules)
 
 # DTBs
