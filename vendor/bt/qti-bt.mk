beabec3e8e5912ccbddc0eefd453074339ebda3a
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-09-08 15:16:53 +0200

    vendor: bt: Add condition to use AIDL QTI bt audio
    Change-Id: If2e83082322709c2acdf2b59f22e23722587e2fd
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/bt/qti-bt.mk
+++ vendor/bt/qti-bt.mk
@@ -20,7 +20,6 @@ endif
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
@@ -29,6 +28,14 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+
+ifeq ($(TARGET_USE_AIDL_QTI_BT_AUDIO),true)
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio-impl
+else
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.1-impl
+endif
 
 # FM
 PRODUCT_PACKAGES += \
