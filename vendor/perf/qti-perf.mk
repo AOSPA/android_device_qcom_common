86a31491984866a15c5b31850eb887491594a057
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-12 08:33:40 +0530

    vendor: perf: Cleanup legacy platforms
    This component is intended to use only with 5.10+.
    While we're at it, also remove 5.10 stuff from perf-legacy.
    
    Change-Id: I76e9d94908745f5e48b1b6f8de8bdab23e1eb37a

--- vendor/perf/qti-perf.mk
+++ vendor/perf/qti-perf.mk
@@ -23,18 +23,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(QCOM_COMMON_PATH)/vendor/perf/configs/$(TARGET_BOARD_PLATFORM),$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,$(QCOM_COMMON_PATH)/vendor/perf/configs/test,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Disable IOP HAL for select platforms.
-ifeq ($(call is-board-platform-in-list, msm8937 msm8953 msm8998 qcs605 sdm660 sdm710),true)
-PRODUCT_COPY_FILES += \
-    $(QCOM_COMMON_PATH)/vendor/perf/vendor.qti.hardware.iop@2.0-service-disable.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.iop@2.0-service-disable.rc
-endif
-
-# Disable the poweropt service for <5.4 platforms.
-ifeq (,$(filter 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
-PRODUCT_COPY_FILES += \
-    $(QCOM_COMMON_PATH)/vendor/perf/poweropt-service-disable.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/poweropt-service-disable.rc
-endif
-
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor \
@@ -43,36 +31,19 @@ PRODUCT_PACKAGES += \
     libtflite \
     vendor.qti.hardware.servicetracker@1.2.vendor
 
-# Only copy task_profiles.json for 5.4 targets.
-ifeq ($(TARGET_KERNEL_VERSION),5.4)
-PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-endif
-
 # Properties
 PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.beluga.p=0x3 \
+    ro.vendor.beluga.c=0x4800 \
+    ro.vendor.beluga.s=0x900 \
+    ro.vendor.beluga.t=0x240 \
     ro.vendor.perf-hal.ver=2.3 \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.perf.scroll_opt=true \
     ro.vendor.qspm.enable=true \
     vendor.perf.framepacing.enable=1 \
-    vendor.power.pasr.enabled=true
-
-ifneq (,$(filter 5.10 5.15, $(TARGET_KERNEL_VERSION)))
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.beluga.p=0x3 \
-    ro.vendor.beluga.c=0x4800 \
-    ro.vendor.beluga.s=0x900 \
-    ro.vendor.beluga.t=0x240
-endif
-
-ifneq (,$(filter 4.14 4.19 5.4 5.10 5.15, $(TARGET_KERNEL_VERSION)))
-ifeq ($(TARGET_BOARD_PLATFORM), holi)
-PRODUCT_VENDOR_PROPERTIES += vendor.pasr.activemode.enabled=false
-else
-PRODUCT_VENDOR_PROPERTIES += vendor.pasr.activemode.enabled=true
-endif
-endif
+    vendor.power.pasr.enabled=true \
+    vendor.pasr.activemode.enabled=true
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/perf/perf-vendor.mk)
