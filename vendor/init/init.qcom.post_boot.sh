b4989576b8dd692759cdff7e42baff78e13b3127
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-15 09:59:28 +0530

    vendor: init: Add custom prop guard in init.qcom.post_boot.sh
    This was missed in waipio addition.
    
    Change-Id: I59f7bc48a2189b284cc431d3e958fc5510032f10

--- vendor/init/init.qcom.post_boot.sh
+++ vendor/init/init.qcom.post_boot.sh
@@ -27,6 +27,11 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+if [[ "$(getprop vendor.post_boot.custom)" == "true" ]]; then
+  echo "Device overrides post_boot, skipping $0"
+  exit 0
+fi
+
 function 8953_sched_dcvs_eas()
 {
     #governor settings
