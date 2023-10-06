886f37001f54284cc65b70ccd84ce9b80bbfd844
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-10-06 16:41:42 +0000

    common: Update configs to LA.QSSI.14.0.r1-09500-qssi.0
    Change-Id: I8b7f3347087ebfe061946acbc4b3c757b8a5f068
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- system/wfd/qti-wfd.mk
+++ system/wfd/qti-wfd.mk
@@ -22,7 +22,7 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    debug.sf.enable_hwc_vds=1 \
+    debug.sf.enable_hwc_vds=0 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
 
