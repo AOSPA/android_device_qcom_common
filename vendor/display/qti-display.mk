b62fe2969342648883902074021670c332f3c08b
Jake Weinstein <jake@aospa.co>
2023-10-07 14:56:51 +0900

    vendor: display: Disable custom content metadata region on <5.15 targets
    Pre-8550 devices do not currently support this.
    
    Change-Id: I9aac7cf293ff314eae6fed594a8307f71f682f32

--- vendor/display/qti-display.mk
+++ vendor/display/qti-display.mk
@@ -62,5 +62,10 @@ PRODUCT_COPY_FILES += \
     device/qcom/common/vendor/display/5.4/feature_enabler_client.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/feature_enabler_client.rc
 endif
 
+# Disable custom content metadata region on <5.15 targets
+ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4 5.10, $(TARGET_KERNEL_VERSION)))
+TARGET_GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE := true
+endif
+
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/display/$(TARGET_KERNEL_VERSION)/display-vendor.mk)
