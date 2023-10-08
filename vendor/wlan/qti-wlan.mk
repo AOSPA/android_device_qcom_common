50b50beb1a7aa6bff6e8c54f13df087863274a91
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-10-08 18:49:18 +0000

    common: Switch to full AIDL wifi
    Change-Id: I03513e7a61cff961514be6f962e7414cbeaf09fb
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/wlan/qti-wlan.mk
+++ vendor/wlan/qti-wlan.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     fstman \
     fstman.ini \
     hostapd \
