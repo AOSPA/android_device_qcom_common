69afe46bb2d50e4e0dd26d930f8e7f0f1fb197a8
Jprimero15 <jprimero15@aospa.co>
2023-10-22 21:02:51 +0800

    common: alarm: copy the manifest instead of appending
    * some dt does not use += on their DEVICE_MANIFEST_FILE because of this it fails to be appended so to be safe just put it on proprietary blobs.
    
    Change-Id: I34efcc0b65beffa9ddbff6585594b1e0d3e2886f
    Signed-off-by: Jprimero15 <jprimero15@aospa.co>

--- vendor/alarm/qti-alarm.mk
+++ vendor/alarm/qti-alarm.mk
@@ -11,9 +11,5 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     init.qcom.alarm.rc
 
-# Manifest
-DEVICE_MANIFEST_FILE += \
-    $(QCOM_COMMON_PATH)/vendor/alarm/alarm-manifest.xml
-
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/vendor/alarm/alarm-vendor.mk)
