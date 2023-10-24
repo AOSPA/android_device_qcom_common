45c89a94bc5c1dc06434a76d9212c52c5a05ff07
Jake Weinstein <jake@aospa.co>
2023-10-24 11:31:53 -0500

    vendor: Introduce adreno-t component variant
    This will be used on kalama and crow.
    
    Change-Id: I3d798e6811b7fc4cf57f273d13b26d593e617876

--- components.mk
+++ components.mk
@@ -37,7 +37,9 @@ endif
 # QTI Common Components
 
 ifneq (,$(filter adreno, $(TARGET_COMMON_QTI_COMPONENTS)))
-  ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY)),true)
+  ifeq ($(call is-board-platform-in-list,$(5_15_FAMILY)),true)
+    TARGET_ADRENO_COMPONENT_VARIANT ?= adreno-t
+  else ifeq ($(call is-board-platform-in-list,$(5_10_FAMILY)),true)
     TARGET_ADRENO_COMPONENT_VARIANT ?= adreno-s
   else ifeq ($(call is-board-platform-in-list,$(4_14_FAMILY) $(4_19_FAMILY) $(5_4_FAMILY)),true)
     TARGET_ADRENO_COMPONENT_VARIANT ?= adreno-r
