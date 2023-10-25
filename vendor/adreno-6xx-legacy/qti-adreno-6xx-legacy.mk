3a8f429b1c39bc3815fdbf67933e71ad107b6e5b
TimoDo <timo.capa@gmail.com>
2023-10-25 21:21:28 +0200

    adreno-6xx-legacy: update to 728 kona
    also grab absent things from 692 and split the proprietary file
    
    Change-Id: I70692e0fa67b0681e94a0a4976ac83f5d6677053

--- vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk
+++ vendor/adreno-6xx-legacy/qti-adreno-6xx-legacy.mk
@@ -20,14 +20,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 # Properties
 PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    graphics.gpu.profiler.support=true
 
 # Get non-open-source specific aspects.
 $(call inherit-product, vendor/qcom/common/vendor/adreno-6xx-legacy/adreno-6xx-legacy-vendor.mk)
