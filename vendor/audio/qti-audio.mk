a0f7b8bd8bae83030828ebc7d86139a6f9c995ea
Jake Weinstein <jake@aospa.co>
2023-10-31 07:06:26 +0900

    vendor: audio: Disable loop on compress read for targets < 5.10
    This change isn't ready for older audio HALs and
    breaks compress audio record, such as in WhatsApp.
    
    Change-Id: I84c8427210f265964d8fdb1818d5b4dcd2abe56a

--- vendor/audio/qti-audio.mk
+++ vendor/audio/qti-audio.mk
@@ -25,6 +25,11 @@ TARGET_USES_RRO := true
 AUDIO_FEATURE_ENABLED_DYNAMIC_LOG := false
 BOARD_SUPPORTS_OPENSOURCE_STHAL := false
 
+# Flags for <5.10 targets
+ifneq (,$(filter 3.18 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
+TARGET_LOOP_COMPRESS_READ := true
+endif
+
 # OMX Packages
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
