b4e3613e556bf7984691bf2aac2fe1913a2a7fa4
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-11-24 17:20:24 +0000

    vendor: charging: Use AIDL QTI Health by default now
    Change-Id: I1b489ae1d9487b4e4101cc7e7314b7173156d10d
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/charging/qti-charging.mk
+++ vendor/charging/qti-charging.mk
@@ -16,7 +16,7 @@ PRODUCT_SOONG_NAMESPACES += \
     device/qcom/common/vendor/charging
 
 # Health
-ifeq ($(TARGET_USE_AIDL_QTI_HEALTH),true)
+ifneq ($(TARGET_USE_HIDL_QTI_HEALTH),true)
 $(call inherit-product, vendor/qcom/opensource/healthd-ext/health-vendor-product.mk)
 
 PRODUCT_PACKAGES += \
