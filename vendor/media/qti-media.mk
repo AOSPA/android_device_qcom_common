bd925edc1b848270ce722ff183529f13078ccc08
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-12-24 01:26:22 +0000

    vendor: media: Allow override media.settings.xml node
    [1] - Qualcomm has many platforms with several variants of the same SoC, and in general media_profiles_vendor is only used for the platform's main SoC, so allow an override of this in the device tree.
    [2] - We could use an override prop in ODM and functions, but this causes double definition recording in the logs, so in SYSTEM_EXT it seems better for now
    
    Change-Id: Ide473af87ae228aa99e0e5702d5cb3638ec2f495
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor/media/qti-media.mk
+++ vendor/media/qti-media.mk
@@ -42,8 +42,8 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Properties
+PRODUCT_SYSTEM_EXT_PROPERTIES ?= media.settings.xml=/vendor/etc/media_profiles_vendor.xml
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
     ro.media.recorder-max-base-layer-fps=60
 
