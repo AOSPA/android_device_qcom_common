bb2b18aeef64ce0981801ecb475250a2ddf4dbcd
Jake Weinstein <jake@aospa.co>
2024-02-04 22:49:06 -0600

    vendor: Import overlay component
    SoC overlays are a combination of S, R, and T GRF.
    
    Change-Id: I1c812ecb864d922b978c84b959e9efd8655d6868

---
+++ vendor/overlay/qti-overlay.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2024 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_SOONG_NAMESPACES += \
+    device/qcom/common/vendor/overlay
+
+# Overlays
+PRODUCT_PACKAGES += \
+    BluetoothResTarget_$(TARGET_BOARD_PLATFORM) \
+    FrameworksResTarget_$(TARGET_BOARD_PLATFORM) \
+    SecureElementResTarget_$(TARGET_BOARD_PLATFORM) \
+    WifiResTarget_$(TARGET_BOARD_PLATFORM) \
+    WifiResTarget_spf_$(TARGET_BOARD_PLATFORM)
+
+ifeq ($(TARGET_BOARD_PLATFORM),lahaina)
+PRODUCT_PACKAGES += \
+    WifiResTarget_yupik_iot
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),taro)
+PRODUCT_PACKAGES += \
+    WifiResTarget_cape
+endif
