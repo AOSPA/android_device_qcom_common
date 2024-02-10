ece6cf2dbf51289f16d1fe0ea1d34f50037fd043
Omkar Chandorkar <gotenksIN@aospa.co>
2024-02-10 23:03:03 +0900

    common: Switch to qssi device_framework_manifest
    - Reduces maintenance
    
    Change-Id: I4558fa90b0374037025dce75e56308d0b95228f6
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/telephony/qti-telephony.mk
+++ system/telephony/qti-telephony.mk
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 # AIDL / HIDL
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(QCOM_COMMON_PATH)/system/telephony/framework_manifest.xml
 SYSTEM_EXT_MANIFEST_FILES += $(QCOM_COMMON_PATH)/system/telephony/atcmdfwd-saidl.xml
 
 # Packages
