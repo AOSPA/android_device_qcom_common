7acbebf002af446eefd5f58eb1bb51bb5e29fcdc
Omkar Chandorkar <gotenksIN@aospa.co>
2024-02-15 17:29:18 +0900

    common: Update to LA.QSSI.14.0.r1-12000-qssi.0
    Change-Id: I34cae8387f57c0818995c7caf03d42c85d20448a
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/perf/qti-perf.mk
+++ system/perf/qti-perf.mk
@@ -20,7 +20,8 @@ PRODUCT_BOOT_JARS += \
 # Packages
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0 \
-    libavservices_minijail
+    libavservices_minijail \
+    vendor.qti.hardware.qspa-V1-ndk
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/system/perf/perf-vendor.mk)
