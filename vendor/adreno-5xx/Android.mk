30e91adfbf017844cce2e52001749b072ed4a1f7
Bruno Martins <bgcngm@gmail.com>
2024-03-28 13:11:44 +0000

    common: adreno: Declare EGL libs as symlinks during extraction
    Change-Id: I4b2f3e79b5e518889d9771bf8990e27babca8b61

--- vendor/adreno-5xx/Android.mk
+++ vendor/adreno-5xx/Android.mk
@@ -18,25 +18,4 @@ ifeq ($(TARGET_ADRENO_COMPONENT_VARIANT),adreno-5xx)
 
 include $(CLEAR_VARS)
 
-EGL_LIBRARIES := \
-	libEGL_adreno.so \
-	libGLESv2_adreno.so \
-	libq3dtools_adreno.so
-
-EGL_32_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_32_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-EGL_64_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/lib64/,$(notdir $(EGL_LIBRARIES)))
-$(EGL_64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf egl/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += \
-	$(EGL_32_SYMLINKS) \
-	$(EGL_64_SYMLINKS)
-
 endif
