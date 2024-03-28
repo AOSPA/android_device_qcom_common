cf3ac63c6c14c8f140c3c72e292f8b8a2a5ed70d
Bruno Martins <bgcngm@gmail.com>
2024-03-28 14:10:14 +0000

    common: telephony: Create symlink target for CneApp
    Change-Id: Ic9629b046e078ca170fe081952749b8e51899ff6

--- vendor/telephony/Android.mk
+++ vendor/telephony/Android.mk
@@ -18,14 +18,4 @@ ifneq (,$(filter telephony, $(TARGET_COMMON_QTI_COMPONENTS)))
 
 include $(CLEAR_VARS)
 
-CNE_LIBRARIES := libvndfwk_detect_jni.qti.so
-
-CNE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/CneApp/lib/arm64/,$(notdir $(CNE_LIBRARIES)))
-$(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS)
-
 endif
