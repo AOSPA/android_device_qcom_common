b76ecec69f2653dde004fe6410a4f9f032d96318
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-04-14 20:03:04 +0000

    vendor: init: Update to LA.VENDOR.14.3.0.r1-09300-lanai.0
    * keep some changes we made
    * keep parrot, crow and kalama on post_boot
    
    Change-Id: I6deeb18aedad6e60e3278a57291d2cea9eb3453a

--- vendor/init/pineapple/bin/init.kernel.post_boot-cliffs.sh
+++ vendor/init/pineapple/bin/init.kernel.post_boot-cliffs.sh
@@ -48,17 +48,67 @@ fallback_setting()
 	done
 }
 
+enable_thp()
+{
+	# THP enablement settings
+	echo always > /sys/kernel/mm/transparent_hugepage/enabled
+
+	MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+	MemTotal=${MemTotalStr:16:8}
+	let RamSizeGB="( $MemTotal / 1048576 ) + 1"
+
+	# Set the min_free_kbytes to standard kernel value
+	if [ $RamSizeGB -ge 8 ]; then
+		MinFreeKbytes=11584
+	elif [ $RamSizeGB -ge 4 ]; then
+		MinFreeKbytes=8192
+	elif [ $RamSizeGB -ge 2 ]; then
+		MinFreeKbytes=5792
+	else
+		MinFreeKbytes=4096
+	fi
+
+	# We store min_free_kbytes into a vendor property so that the PASR
+	# HAL can read and set the value for it.
+	echo $MinFreeKbytes > /proc/sys/vm/min_free_kbytes
+	setprop vendor.memory.min_free_kbytes $MinFreeKbytes
+
+	#Enable the PASR support
+	ddr_type=`od -An -tx /proc/device-tree/memory/ddr_device_type`
+	ddr_type5="08"
+
+	if [ -d /sys/kernel/mem-offline ]; then
+		#only LPDDR5 supports PAAR
+		if [ ${ddr_type:4:2} != $ddr_type5 ]; then
+			setprop vendor.pasr.activemode.enabled false
+		fi
+		setprop vendor.pasr.enabled true
+	fi
+}
+
+case "$2" in
+	"632"|"643")
+		#THP is disabled by default. Do nothing here.
+		;;
+	*)
+		enable_thp
+		;;
+esac
+
 variant=$(get_num_logical_cores_in_physical_cluster "$1")
 echo "CPU topology: ${variant}"
 case "$variant" in
-	"2_3_2_1")
-	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-pineapple_default_2_3_2_1.sh
+	"3_4_1")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-cliffs_default_3_4_1.sh
 	;;
-	"2_3_1_1")
-	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-pineapple_2_3_1_1.sh
+	"3_3_1")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-cliffs_3_3_1.sh
 	;;
-	"2_3_2_0")
-	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-pineapple_2_3_2_0.sh
+	"2_3_0")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-cliffs_2_3_0.sh
+	;;
+	"2_2_1")
+	vendor/bin/sh /vendor/bin/init.kernel.post_boot-cliffs_2_2_1.sh
 	;;
 	*)
 	echo "***WARNING***: Postboot script not present for the variant ${variant}"
