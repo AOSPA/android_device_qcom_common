749a128d3c2d78c58d5f6fa8f39c442dcbab4c4c
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    common: wfd: Declare WFD libs as symlinks during extraction
    Change-Id: I2a035323a483b8b2422b4a54219b2f6268ab4a61

--- system/wfd/Android.mk
+++ system/wfd/Android.mk
@@ -18,14 +18,4 @@ ifeq ($(TARGET_WFD_COMPONENT_VARIANT),wfd)
 
 include $(CLEAR_VARS)
 
-WFDSERVICE_LIBRARIES := libwfdnative.so
-
-WFDSERVICE_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/WfdService/lib/arm64/,$(notdir $(WFDSERVICE_LIBRARIES)))
-$(WFDSERVICE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/system_ext/lib64/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WFDSERVICE_SYMLINKS)
-
 endif
