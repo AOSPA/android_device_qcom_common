a444ab35c8dbad83f4fe1d8402a5aec36925e127
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-07-19 18:39:01 +0530

    TEST
    Change-Id: Icb8edfe114bdaf1b6f052e9a8ee2a1d23a035451
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- vendor/nfc/qti-nfc.mk
+++ vendor/nfc/qti-nfc.mk
@@ -9,28 +9,7 @@ TARGET_USES_NQ_NFC ?= true
 ifeq ($(TARGET_USES_NQ_NFC), true)
 
 # Inherit from NQ NFC.
-$(call inherit-product, vendor/nxp/opensource/commonsys/packages/apps/Nfc/nfc_system_product.mk)
-$(call inherit-product, vendor/nxp/opensource/halimpl/nfc_vendor_product.mk)
-
-# Packages
-PRODUCT_PACKAGES += \
-    se_nq_extn_client \
-    ls_nq_client \
-    jcos_nq_client
-
-# Permissions
-ifneq ($(TARGET_NFC_SKU),)
-NFC_PERMISSIONS_DIR := $(TARGET_COPY_OUT_ODM)/etc/permissions/sku_$(TARGET_NFC_SKU)
-else
-NFC_PERMISSIONS_DIR := $(TARGET_COPY_OUT_VENDOR)/etc/permissions
-endif
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(NFC_PERMISSIONS_DIR)/android.hardware.se.omapi.ese.xml \
-    frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(NFC_PERMISSIONS_DIR)/android.hardware.se.omapi.uicc.xml
-
-# Get non-open-source specific aspects.
-$(call inherit-product-if-exists, vendor/qcom/common/vendor/nfc/nq-nfc-vendor.mk)
+$(call inherit-product, device/qcom/common/vendor/nfc/nq/qti-nfc.mk)
 
 else
 
