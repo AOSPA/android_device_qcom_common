1e4394406aed38fb31bbed0a6a2b996729ac5746
Omkar Chandorkar <gotenksIN@aospa.co>
2024-09-10 21:53:22 +0900

    vendor: usb: Enable QTI Gadget HAL for 5.10+
    Change-Id: Ic1ecf9d38c4b104469255d929f0b852eabd90bc5
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/usb/qti-usb.mk
+++ vendor/usb/qti-usb.mk
@@ -26,6 +26,10 @@ ifneq (,$(filter 4.4 4.9 4.14, $(TARGET_KERNEL_VERSION)))
 PRODUCT_PACKAGES += android.hardware.usb@1.0-service
 endif
 
+ifeq (,$(filter 4.4 4.9 4.14 4.19 5.4, $(TARGET_KERNEL_VERSION)))
+PRODUCT_HAS_GADGET_HAL := true
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
