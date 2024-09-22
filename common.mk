c20af2ba5fc79f0a406c214e75cc916eb305c9f3
Omkar Chandorkar <gotenksIN@aospa.co>
2024-09-23 02:27:05 +0900

    common: Build ex-vndk libs for <V vendor
    Change-Id: Ic3d7a5732c4efc6e14424d1a6596880f41074cd5
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- common.mk
+++ common.mk
@@ -169,7 +169,8 @@ TARGET_FS_CONFIG_GEN += $(QCOM_COMMON_PATH)/config.fs
 
 # GPS
 PRODUCT_PACKAGES += \
-    libcurl
+    libcurl \
+    libcurl.vendor
 
 # Media
 TARGET_DYNAMIC_64_32_MEDIASERVER := true
@@ -209,6 +210,14 @@ PRODUCT_PACKAGES += \
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager
+
+# VNDK
+PRODUCT_PACKAGES += \
+    libexif.vendor \
+    libjsoncpp.vendor \
+    libpng.vendor \
+    libsqlite.vendor \
+    libutilscallstack.vendor
 
 # SoC
 PRODUCT_VENDOR_PROPERTIES += \
