f740d709983f1b6116dd4ba97fbbd1ea8e6080f9
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-10-08 16:55:04 +0000

    system: av: Build libaudiohalvendorextn from OSS
    Change-Id: I4855f0e9ea86fd322aa6415086fcfcae9f93cddc
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- system/av/qti-av.mk
+++ system/av/qti-av.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Audio
+PRODUCT_PACKAGES += \
+    libaudiohalvendorextn
+
 # Enable QCT resampler
 AUDIO_FEATURE_ENABLED_EXTN_RESAMPLER := true
 
