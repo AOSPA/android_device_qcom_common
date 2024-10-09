8cca9d1b68cc53cb9f4fffcd101d042937b502a0
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-09 20:05:54 +0530

    common: Sync extract-utils with template
    Change-Id: I84dda8683eef92ac949b27ea06c1915a3ef61575
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/display/4.14/extract-files.sh
+++ vendor/display/4.14/extract-files.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 #
-# Copyright (C) 2021 Paranoid Android
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2020 Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
 #
 
 # If we're being sourced by the common script that we called,
@@ -23,9 +14,9 @@ fi
 
 set -e
 
-# Required!
 export COMPONENT=display
 export VENDOR=qcom/common/vendor
+export VENDOR_COMMON=${VENDOR}
 export KERNEL_VERSION=4.14
 
 "../../extract-files.sh" "$@"
