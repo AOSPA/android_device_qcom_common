afb49478c883b15748197340aa47a6e410777d3e
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-10-27 08:31:17 +0530

    [DNM] common: Add support for extracting QTI common components
     An extension of https://github.com/LineageOS/android_tools_extract-utils/tree/lineage-22.0/extract_utils
    
    Change-Id: I9e2ec9a5869a85287793e1dbaca1883b83b9f8d2
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

---
+++ extract_utils/__init__.py
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2024 Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from qti import ExtractUtilsQTIModule, QTIComponentType
+
+__all__ = [
+    'ExtractUtilsQTIModule',
+    'QTIComponentType',
+]
---
+++ system/telephony/extract-files.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S PYTHONPATH=../../extract_utils:../../../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2024 Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from qti import ExtractUtilsQTIModule, QTIComponentType
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import ExtractUtils
+
+blob_fixups: blob_fixups_user_type = {
+    ('product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml', 'product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml', 'product/etc/permissions/vendor.qti.hardware.data.connectionaidl-V1-java.xml'): blob_fixup()
+        .regex_replace('xml version="2.0"', 'xml version="1.0"'),
+}  # fmt: skip
+
+module = ExtractUtilsQTIModule(
+    'telephony',
+    QTIComponentType.SYSTEM,
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
