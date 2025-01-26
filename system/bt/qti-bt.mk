ddf69d161711039e40180d1a9c1376976ca109aa
xenxynon <flynryder427@gmail.com>
2025-01-26 14:47:10 +0000

    system: bt: cleanup properties
    Change-Id: I092963715cd2aa9c95b792254222164690c1467d

--- system/bt/qti-bt.mk
+++ system/bt/qti-bt.mk
@@ -18,12 +18,5 @@ $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commo
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-system-opensource-product.mk)
 endif
 
-# Properties
-ifneq ($(TARGET_USE_QTI_BT_STACK),false)
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    persist.vendor.bt.a2dp.aac_whitelist=false \
-    ro.bluetooth.library_name=libbluetooth_qti.so
-endif
-
 # Get non-open-source specific aspects.
 $(call inherit-product-if-exists, vendor/qcom/common/system/bt/bt-vendor.mk)
