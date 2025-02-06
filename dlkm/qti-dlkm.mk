37f13fbfb13c447523da1bc0c4198c1da0b2dded
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-06 12:44:19 +0100

    fixup!: common: dlkm: Update modules include for 5.15
    Change-Id: Ic634051b696fc7161403ce4bc16bfd6d7842d3b4

--- dlkm/qti-dlkm.mk
+++ dlkm/qti-dlkm.mk
@@ -37,8 +37,8 @@ include $(QCOM_COMMON_PATH)/dlkm/kernel-platform.mk
 -include vendor/qcom/opensource/display-drivers/display_driver_product.mk
 
 # eSE
--include vendor/st/opensource/ese-kernel/ese_kernel_board.mk
--include vendor/st/opensource/ese-kernel/ese_kernel_product.mk
+-include vendor/st/opensource/eSE-driver/ese_kernel_board.mk
+-include vendor/st/opensource/eSE-driver/ese_kernel_product.mk
 
 # EVA
 -include vendor/qcom/opensource/eva-kernel/eva_kernel_board.mk
