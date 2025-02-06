598ff299d93ef9fd6d12b7c22e725db9323b1aaa
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-02-06 14:33:08 +0100

    common: dlkm: Dynamically pickup product & board definitions
    Change-Id: I7ea0cd3cc9b3a52ba7d569dc0edaec3c74167b03

--- dlkm/qti-dlkm.mk
+++ dlkm/qti-dlkm.mk
@@ -5,70 +5,7 @@
 
 include $(QCOM_COMMON_PATH)/dlkm/kernel-platform.mk
 
-# Audio
--include vendor/qcom/opensource/audio-kernel/audio_kernel_product_board.mk
--include vendor/qcom/opensource/audio-kernel/audio_kernel_vendor_board.mk
-
-# BT
--include vendor/qcom/opensource/bt-kernel/bt_kernel_product_board.mk
--include vendor/qcom/opensource/bt-kernel/bt_kernel_vendor_board.mk
-
-# Camera
--include vendor/qcom/opensource/camera-kernel/board.mk
--include vendor/qcom/opensource/camera-kernel/product.mk
-
-# CVP
--include vendor/qcom/opensource/cvp-kernel/cvp_kernel_board.mk
--include vendor/qcom/opensource/cvp-kernel/cvp_kernel_product.mk
-
-# Dataipa
--include vendor/qcom/opensource/dataipa/dataipa_dlkm_vendor_board.mk
--include vendor/qcom/opensource/dataipa/dataipa_dlkm_vendor_product.mk
-
-# Datarmnet
--include vendor/qcom/opensource/datarmnet/datarmnet_dlkm_vendor_board.mk
--include vendor/qcom/opensource/datarmnet/datarmnet_dlkm_vendor_product.mk
--include vendor/qcom/opensource/datarmnet-ext/datarmnet_ext_dlkm_vendor_board.mk
--include vendor/qcom/opensource/datarmnet-ext/datarmnet_ext_dlkm_vendor_product.mk
-
-# Display
--include vendor/qcom/opensource/display-drivers/display_driver_board.mk
--include vendor/qcom/opensource/display-drivers/display_driver_product.mk
-
-# eSE
--include vendor/st/opensource/eSE-driver/ese_kernel_board.mk
--include vendor/st/opensource/eSE-driver/ese_kernel_product.mk
-
-# EVA
--include vendor/qcom/opensource/eva-kernel/eva_kernel_board.mk
--include vendor/qcom/opensource/eva-kernel/eva_kernel_product.mk
-
-# Graphics
--include vendor/qcom/opensource/graphics-kernel/gfx_driver_product.mk
--include vendor/qcom/opensource/graphics-kernel/gfx_kernel_board.mk
-
-# MM
--include vendor/qcom/opensource/mm-drivers/mm_driver_board.mk
--include vendor/qcom/opensource/mm-drivers/mm_driver_product.mk
-
-# MMRM
--include vendor/qcom/opensource/mmrm-driver/mmrm_kernel_board.mk
--include vendor/qcom/opensource/mmrm-driver/mmrm_kernel_product.mk
-
-# NFC
--include vendor/nxp/opensource/driver/nfc_kernel_dlkm_vendor_board.mk
--include vendor/nxp/opensource/driver/nfc_kernel_dlkm_vendor_product.mk
--include vendor/st/opensource/driver/nfc_kernel_dlkm_vendor_board.mk
--include vendor/st/opensource/driver/nfc_kernel_dlkm_vendor_product.mk
-
-# SecureMSM
--include vendor/qcom/opensource/securemsm-kernel/securemsm_kernel_product_board.mk
--include vendor/qcom/opensource/securemsm-kernel/securemsm_kernel_vendor_board.mk
-
-# Touch
--include vendor/qcom/opensource/touch-drivers/touch_driver_board.mk
--include vendor/qcom/opensource/touch-drivers/touch_driver_product.mk
-
-# Video
--include vendor/qcom/opensource/video-driver/video_kernel_board.mk
--include vendor/qcom/opensource/video-driver/video_kernel_product.mk
+$(foreach vbdefs, $(sort $(wildcard vendor/qcom/defs/board-defs/vendor/*.mk)), \
+    $(call inherit-product, $(vbdefs)))
+$(foreach vpdefs, $(sort $(wildcard vendor/qcom/defs/product-defs/vendor/*.mk)), \
+    $(call inherit-product, $(vpdefs)))
