1b03c67560df275b5c80334df234ee6dc89ed0a5
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2025-03-03 16:50:11 +0300

    common: vendor: Introduce 5.15 media component
    Change-Id: Ie4c3942cc85f50478c2a99aa0b7072c52c6e99cf
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

---
+++ vendor/media/5.15/extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../:../../../../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import ExtractUtils
+
+from extract_utils_qti.module import ExtractUtilsQTIModule, QTIComponentType
+
+namespace_imports = [
+    'hardware/qcom/display',
+    'hardware/qcom/display/gralloc',
+    'vendor/qcom/common/vendor/media/5.15',
+]
+
+module = ExtractUtilsQTIModule(
+    'media/5.15',
+    QTIComponentType.VENDOR,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
