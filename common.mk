ab5828f7baf4edba73c289be8ba776b7e94cb826
Bruno Martins <bgcngm@gmail.com>
2025-04-12 01:23:18 +0100

    common: rfs: Add MDM and MSM OIS symlinks
    As seen on sun targets.
    
    Change-Id: I43e6a43b304e9a582091556d62e3f800e789020e

--- common.mk
+++ common.mk
@@ -257,6 +257,15 @@ PRODUCT_PACKAGES += \
     rfs_mdm_mpss_readwrite_symlink \
     rfs_mdm_mpss_shared_symlink
 
+# RFS MDM OIS symlinks
+PRODUCT_PACKAGES += \
+    rfs_mdm_ois_hlos_symlink \
+    rfs_mdm_ois_ramdumps_symlink \
+    rfs_mdm_ois_readonly_firmware_symlink \
+    rfs_mdm_ois_readonly_vendor_firmware_symlink \
+    rfs_mdm_ois_readwrite_symlink \
+    rfs_mdm_ois_shared_symlink
+
 # RFS MDM SLPI symlinks
 PRODUCT_PACKAGES += \
     rfs_mdm_slpi_hlos_symlink \
@@ -310,6 +319,15 @@ PRODUCT_PACKAGES += \
     rfs_msm_mpss_readonly_vendor_firmware_symlink \
     rfs_msm_mpss_readwrite_symlink \
     rfs_msm_mpss_shared_symlink
+
+# RFS MSM OIS symlinks
+PRODUCT_PACKAGES += \
+    rfs_msm_ois_hlos_symlink \
+    rfs_msm_ois_ramdumps_symlink \
+    rfs_msm_ois_readonly_firmware_symlink \
+    rfs_msm_ois_readonly_vendor_firmware_symlink \
+    rfs_msm_ois_readwrite_symlink \
+    rfs_msm_ois_shared_symlink
 
 # RFS MSM SLPI symlinks
 PRODUCT_PACKAGES += \
