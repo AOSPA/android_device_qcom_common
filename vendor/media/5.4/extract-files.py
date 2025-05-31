86e0b329ff00289018eb5cb678f7236d7abfe6fe
Omkar Chandorkar <gotenksIN@aospa.co>
2025-05-31 10:48:15 +0530

    vendor: media: Split component based on kernel version
    Change-Id: Ie3dbe245291f6d9457816a45ed4bd58a3bee1c2f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/media/5.4/extract-files.py
+++ vendor/media/5.4/extract-files.py
@@ -1,39 +1,32 @@
-#!/usr/bin/env -S PYTHONPATH=../../:../../../../../tools/extract-utils python3
+#!/usr/bin/env -S PYTHONPATH=../../../:../../../../../../tools/extract-utils python3
 #
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-FileCopyrightText: Paranoid Android
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.fixups_lib import (
-    lib_fixup_remove,
-    lib_fixups,
-    lib_fixups_user_type,
-)
+from extract_utils.fixups_lib import lib_fixups, lib_fixups_user_type
 from extract_utils.main import ExtractUtils
 
+from extract_utils_qti.fixups_lib import lib_fixup_vendor_suffix
 from extract_utils_qti.module import ExtractUtilsQTIModule, QTIComponentType
 
 namespace_imports = [
     'hardware/qcom/display',
-    'vendor/qcom/common/vendor/dsprpcd',
+    'hardware/qcom/display/gralloc',
 ]
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
-        'libc2dcolorconvert',
-        'libmmosal',
-        'libOmxCore',
-        'libplatformconfig',
-        'libwfdcommonutils_proprietary',
-        'libwfdmmservice_proprietary',
-        'libwfdutils_proprietary',
-    ): lib_fixup_remove,
+        'vendor.qti.hardware.perf@2.0',
+        'vendor.qti.hardware.perf@2.1',
+        'vendor.qti.hardware.perf@2.2',
+    ): lib_fixup_vendor_suffix,
 }
 
 module = ExtractUtilsQTIModule(
-    'media-legacy',
+    'media/5.4',
     QTIComponentType.VENDOR,
     namespace_imports=namespace_imports,
     lib_fixups=lib_fixups,
