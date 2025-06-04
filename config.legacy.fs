8b2b7949e705d491271293e86f95fc04d0b91d57
Omkar Chandorkar <gotenksIN@aospa.co>
2025-06-04 08:57:27 +0530

    config.fs: Update to LA.VENDOR.15.4.0.r1-17000-pakala.0
    Change-Id: I5aa8037cb27dd37c0e40e17b8d5b2a746a47b16e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ config.legacy.fs
@@ -0,0 +1,23 @@
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
+mode: 0755
+user: AID_VENDOR_NXP_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.weaver@1.0-service]
+mode: 0755
+user: AID_VENDOR_NXP_WEAVER
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.bluetooth@1.1-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/hw/android.hardware.bluetooth@1.1-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
