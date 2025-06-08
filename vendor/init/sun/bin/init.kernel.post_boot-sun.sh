889fad150dbceab855dc3e013caa20d98488336d
Omkar Chandorkar <gotenksIN@aospa.co>
2025-06-08 13:54:30 +0900

    vendor: init: Update kernel scripts from LA.VENDOR.15.4.0.r1-10800-pakala.0
    Change-Id: Ifd2f56e70595db34bea02b5b0b19bbb176c2640d
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- vendor/init/sun/bin/init.kernel.post_boot-sun.sh
+++ vendor/init/sun/bin/init.kernel.post_boot-sun.sh
@@ -1,49 +1,67 @@
 #=============================================================================
-# Copyright (c) 2020-2023 Qualcomm Technologies, Inc.
+# Copyright (c) 2024 Qualcomm Technologies, Inc.
 # All Rights Reserved.
 # Confidential and Proprietary - Qualcomm Technologies, Inc.
-#
-# Copyright (c) 2009-2012, 2014-2019, The Linux Foundation. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of The Linux Foundation nor
-#       the names of its contributors may be used to endorse or promote
-#       products derived from this software without specific prior written
-#       permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #=============================================================================
 
-rev=`cat /sys/devices/soc0/revision`
+get_num_logical_cores_in_physical_cluster()
+{
+	i=0
+	logical_cores=(0 0 0 0 0 0)
+	if [ -f /sys/devices/system/cpu/cpu0/topology/cluster_id ] ; then
+		physical_cluster="cluster_id"
+	else
+		physical_cluster="physical_package_id"
+	fi
+	for i in `ls -d /sys/devices/system/cpu/cpufreq/policy[0-9]*`
+	do
+		if [ -e $i ] ; then
+			num_cores=$(cat $i/related_cpus | wc -w)
+			first_cpu=$(echo "$i" | sed 's/[^0-9]*//g')
+			cluster_id=$(cat /sys/devices/system/cpu/cpu$first_cpu/topology/$physical_cluster)
+			logical_cores[cluster_id]=$num_cores
+		fi
+	done
+	cpu_topology=""
+	j=0
+	physical_cluster_count=$1
+	while [[ $j -lt $physical_cluster_count ]]; do
+		cpu_topology+=${logical_cores[$j]}
+		if [ $j -lt $physical_cluster_count-1 ]; then
+			cpu_topology+="_"
+		fi
+		j=$((j+1))
+	done
+	echo $cpu_topology
+}
 
-echo 4 > /proc/sys/kernel/printk
+#Implementing this mechanism to jump to powersave governor if the script is not running
+#as it would be an indication for devs for debug purposes.
+fallback_setting()
+{
+	governor="powersave"
+	for i in `ls -d /sys/devices/system/cpu/cpufreq/policy[0-9]*`
+	do
+		if [ -f $i/scaling_governor ] ; then
+			echo $governor > $i/scaling_governor
+		fi
+	done
+}
 
-# Change console log level as per console config property
-console_config=`getprop persist.vendor.console.silent.config`
-case "$console_config" in
-	"1")
-		echo "Enable console config to $console_config"
-		echo 0 > /proc/sys/kernel/printk
+variant=$(get_num_logical_cores_in_physical_cluster "$1")
+echo "CPU topology: ${variant}"
+case "$variant" in
+	"6_2")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-sun_default_6_2.sh
+	;;
+	"6_0")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-sun_6_0.sh
+	;;
+	"5_2")
+	/vendor/bin/sh /vendor/bin/init.kernel.post_boot-sun_5_2.sh
 	;;
 	*)
-		echo "Enable console config to $console_config"
+	echo "***WARNING***: Postboot script not present for the variant ${variant}"
+	fallback_setting
 	;;
 esac
-
-setprop vendor.post_boot.parsed 1
