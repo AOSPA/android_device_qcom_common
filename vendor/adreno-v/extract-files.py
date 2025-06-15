7d3ece3e40347d48a99f41292bb079ac49c2ba2f
BladeRunner-A2C <creator4983@aospa.co>
2025-06-15 14:30:10 +0200

    vendor: Introduce adreno-v component
    From QRD8750 firmware
    Based on tag: LA.VENDOR.15.4.0.r1-15000-pakala.0-1
    Adreno Version 800.31.10
    
    Change-Id: I9dfe80518e58c11130acee41685e06c10e7de764

---
+++ vendor/adreno-v/extract-files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S PYTHONPATH=../../:../../../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import lib_fixups, lib_fixups_user_type
+from extract_utils.main import ExtractUtils
+
+from extract_utils_qti.fixups_lib import lib_fixup_vendor_suffix
+from extract_utils_qti.module import ExtractUtilsQTIModule, QTIComponentType
+
+namespace_imports = [
+    'hardware/qcom/display/gralloc',
+    'vendor/qcom/common/vendor/perf',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    'vendor.qti.qspmhal-V1-ndk': lib_fixup_vendor_suffix,
+}
+
+module = ExtractUtilsQTIModule(
+    'adreno-v',
+    QTIComponentType.VENDOR,
+    namespace_imports=namespace_imports,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
