ecbc98470aba5e78bbb79b9f2aa2a62b4bb6520e
Fazil Sheik <fazilsheik@aospa.co>
2025-07-05 03:17:14 +0530

    system: wfd: Patch libwfdservice to resolve duplicate dependencies
    Change-Id: Ieb05fa84098a2804fa9801553d90b035903b7741
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- system/wfd/extract-files.py
+++ system/wfd/extract-files.py
@@ -20,7 +20,7 @@ blob_fixups: blob_fixups_user_type = {
         'system_ext/lib64/libwfdservice.so',
     ): blob_fixup()
         .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so')
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V3-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
 }  # fmt: skip
 
 module = ExtractUtilsQTIModule(
