c39b7b37f35d18da66f997e715bcef94406d523d
Fazil Sheik <fazilsheik@aospa.co>
2025-07-12 15:48:55 +0530

    patch few blobs with libaudioclient_shim
    Change-Id: Ib25c14c302ae69232ab592619b1fbdf60af562cf
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- system/wfd/extract-files.py
+++ system/wfd/extract-files.py
@@ -20,7 +20,8 @@ blob_fixups: blob_fixups_user_type = {
         'system_ext/lib64/libwfdservice.so',
     ): blob_fixup()
         .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so')
-        .replace_needed('android.media.audio.common.types-V3-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V3-cpp.so', 'android.media.audio.common.types-V4-cpp.so')
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
 }  # fmt: skip
