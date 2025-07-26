d2ae2c5dfd6918b0d3cd05c4bf99479b8f353c17
Alexander Koskovich <akoskovich@pm.me>
2025-07-26 04:38:11 -0400

    common: Add a small service to control NrConfig
    Default state is 5G NSA, and pinned there unless NrConfig is changed.
    
    Test: Observe switch to 5G SA when available and ability to make VoNR calls
    Change-Id: Iecd86c67e7bdbe0f4d35e0c0dd8b4f4b4c1eb7cd
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ system/telephony/qtitelephony-compat-service/src/org/lineageos/qtitelephonyservice/compat/BootReceiver.kt
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.qtitelephonyservice.compat
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val service = Intent(context, QtiTelephonyServiceCompat::class.java)
+        context.startService(service)
+    }
+}
