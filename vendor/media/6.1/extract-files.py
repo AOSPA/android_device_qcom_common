dfc2d885d1c2d08d910fe95a2fbf9a378ac19caa
BladeRunner-A2C <creator4983@aospa.co>
2025-07-31 23:31:07 +0200

    vendor: media: Import 6.1 component
    from LA.VENDOR.14.3.0.r1-11400-lanai.0
    
    Change-Id: I072f227372c98378ab8a7bdf2160ba781a1e3f2f
    Signed-off-by: BladeRunner-A2C <creator4983@aospa.co>

---
+++ vendor/media/6.1/extract-files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S PYTHONPATH=../../../:../../../../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import ExtractUtils
+
+from extract_utils_qti.module import ExtractUtilsQTIModule, QTIComponentType
+
+namespace_imports = [
+    'hardware/qcom/display',
+    'hardware/qcom/display/gralloc',
+    'vendor/qcom/common/vendor/media/6.1',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libpalclient',
+        'libvmmem',
+    ): lib_fixup_remove,
+}
+
+module = ExtractUtilsQTIModule(
+    'media/6.1',
+    QTIComponentType.VENDOR,
+    namespace_imports=namespace_imports,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
