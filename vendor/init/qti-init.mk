e69c3b7d0a78c6dacc5bb55e2c1e7e69d0c1d4dc
Fazil Sheik <fazilsheik@aospa.co>
2025-08-01 16:18:59 +0530

    vendor: init: Update kernel-scripts to LA.VENDOR.15.4.0.r1-15000-pakala.0-1
    Change-Id: I1e36c03c3b8fb8059037fc306d9118a99a6c9a40
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

--- vendor/init/qti-init.mk
+++ vendor/init/qti-init.mk
@@ -27,6 +27,8 @@ PRODUCT_PACKAGES += \
     init.qcom.aospa.rc \
     init.class_main.sh \
     init.crda.sh \
+    init.kernel.init_boot-memory.sh \
+    init.kernel.post_boot-memory \
     init.mdm.sh \
     init.qcom.class_core.sh \
     init.qcom.coex.sh \
--- vendor/init/vendor_modprobe.sh
+++ vendor/init/vendor_modprobe.sh
@@ -23,29 +23,27 @@ do
 	if [ "$audio_arch" == "audioreach" ]; then
 		if [ -e ${dir}/modules.audio.ar.blocklist ]; then
 			audio_blocklist_expr="$(sed -n -e 's/blocklist \(.*\)/\1/p' ${dir}/modules.audio.ar.blocklist | sed -e 's/-/_/g' -e 's/^/-e /')"
-		else
-			audio_blocklist_expr="-e %"
-		fi
-	elif [ "$audio_arch" == "audioreach_vio" ]; then
-		if [ -e ${dir}/modules.audio.ar_vio.blocklist ]; then
-			audio_blocklist_expr="$(sed -n -e 's/blocklist \(.*\)/\1/p' ${dir}/modules.audio.ar_vio.blocklist | sed -e 's/-/_/g' -e 's/^/-e /')"
-		else
-			audio_blocklist_expr="-e %"
 		fi
 	else
 		if [ -e ${dir}/modules.audio.legacy.blocklist ]; then
 			audio_blocklist_expr="$(sed -n -e 's/blocklist \(.*\)/\1/p' ${dir}/modules.audio.legacy.blocklist | sed -e 's/-/_/g' -e 's/^/-e /')"
-		else
-			audio_blocklist_expr="-e %"
 		fi
+	fi
+
+	# Use pattern if block list is empty so that all modules pass through grep below
+	if [ "X${audio_blocklist_expr}" = "X" ]; then
+		audio_blocklist_expr="-e %"
 	fi
 
 	if [ -e ${dir}/modules.blocklist ]; then
 		blocklist_expr="$(sed -n -e 's/blocklist \(.*\)/\1/p' ${dir}/modules.blocklist | sed -e 's/-/_/g' -e 's/^/-e /')"
-	else
-		# Use pattern that won't be found in modules list so that all modules pass through grep below
+	fi
+
+	# Use pattern if block list is empty so that all modules pass through grep below
+	if [ "X${blocklist_expr}" = "X" ]; then
 		blocklist_expr="-e %"
 	fi
+
 	# Filter out modules in blocklist - we would see unnecessary errors otherwise
 	load_modules=$(sed = ${dir}/modules.load | sed 'N;s/\n/\t/' | sort -uk2 | sort -nk1 | cut -f2- | grep -w -v ${blocklist_expr} | grep -w -v ${audio_blocklist_expr})
 	first_module=$(echo ${load_modules} | cut -d " " -f1)
