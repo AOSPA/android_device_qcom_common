1933a54f0d768bdc1dd8c2cfa7838e94a8ab6436
Omkar Chandorkar <gotenksIN@aospa.co>
2025-08-04 22:34:03 +0900

    system: Update to LA.QSSI.16.0.r1-06700-qssi.0
    Change-Id: I4171460d38fc724b1f79269e7ce61470bcd1c5bf
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/wfd/extract-files.py
+++ system/wfd/extract-files.py
@@ -14,20 +14,10 @@ namespace_imports = [
     'vendor/qcom/common/system/av',
 ]
 
-blob_fixups: blob_fixups_user_type = {
-    (
-        'system_ext/lib/libwfdservice.so',
-        'system_ext/lib64/libwfdservice.so',
-    ): blob_fixup()
-        .replace_needed('android.hardware.common-V2-ndk_platform.so', 'android.hardware.common-V2-ndk.so')
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
-}  # fmt: skip
-
 module = ExtractUtilsQTIModule(
     'wfd',
     QTIComponentType.SYSTEM,
     namespace_imports=namespace_imports,
-    blob_fixups=blob_fixups,
 )
 
 if __name__ == '__main__':
