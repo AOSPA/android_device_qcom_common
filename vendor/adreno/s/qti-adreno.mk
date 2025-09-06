d3f8144dbe592bbe5172d767a88f27875af95c6b
Jake Weinstein <jake@aospa.co>
2025-09-07 05:24:50 +0900

    common: vendor: Merge adreno components into one directory
    Make things common where they can be
    
    Change-Id: I210068c691a10ce769fb37200816d496cc2eff31

---
+++ vendor/adreno/s/qti-adreno.mk
@@ -0,0 +1,13 @@
+#
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+# Properties
+PRODUCT_VENDOR_PROPERTIES += \
+    graphics.gpu.profiler.support=true
