45819981271e88176f40033a75aaa630f156a3df
Jake Weinstein <jake@aospa.co>
2025-09-07 01:57:30 +0900

    common: Remove support for platforms below 5.4
    Change-Id: I4378c71a945f90a4c1011038ce38f3c85aff46ad

--- vendor/qseecomd-legacy/qti-qseecomd-legacy.mk
+++
@@ -1,7 +0,0 @@
-#
-# SPDX-FileCopyrightText: Paranoid Android
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Get non-open-source specific aspects.
-$(call inherit-product-if-exists, vendor/qcom/common/vendor/qseecomd-legacy/qseecomd-legacy-vendor.mk)
