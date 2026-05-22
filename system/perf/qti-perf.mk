0b48bb4c196ac445d1a9483edb6c3eb8e1678a27
Omkar Chandorkar <gotenksIN@aospa.co>
2026-05-22 20:55:28 +0900

    system: perf: Allowlist workloadclassifier for background service starts
    com.qualcomm.qti.workloadclassifier crashes with
    BackgroundServiceStartNotAllowedException when trying to
    start WLCService from the background:
    
      "Not allowed to start service Intent {
         act=com.qualcomm.qti.workloadclassifier.APP_LAUNCH
         pkg=com.qualcomm.qti.workloadclassifier
         cmp=com.qualcomm.qti.workloadclassifier/.WLCService
       }: app is in background uid"
    
    Add it to the power-save allowlist via sysconfig so it can
    start services even when not in the foreground.
    
    Change-Id: I721d468b906eb8c9f7ba7ba1b2a14a45b52bc885
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/perf/qti-perf.mk
+++ system/perf/qti-perf.mk
@@ -8,5 +8,9 @@ PRODUCT_BOOT_JARS += \
     QPerformance \
     UxPerformance
 
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    device/qcom/common/system/perf/aospa_qti_whitelist_system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/aospa_qti_whitelist_system_ext.xml
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/qcom/common/system/perf/perf-vendor.mk)
