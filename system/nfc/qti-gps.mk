c593c87e0250acf90e301f890bb9be306ac8b061
Omkar Chandorkar <gotenksIN@aospa.co>
2026-07-19 15:28:42 +0000

    system: Update to LA.QSSI.17.0.r1-06700-qssi.0
    Change-Id: Ifa1c68fac55683db839c8e84991b553f9dc43032
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ system/nfc/qti-gps.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: Paranoid Android
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Get non-open-source specific aspects.
+$(call inherit-product-if-exists, vendor/qcom/common/system/nfc/nfc-vendor.mk)
