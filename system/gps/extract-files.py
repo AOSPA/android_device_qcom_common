c5ebdfe449bf630dd561e8d3217d08e435f21057
Omkar Chandorkar <gotenksIN@aospa.co>
2026-08-31 04:48:24 +0000

    system: Update to LA.QISI.17.0.r1-02700-qssi.0
    Change-Id: I64cbbe5a5e99f1e1808e403c64e7500e7d797f66
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/gps/extract-files.py
+++ system/gps/extract-files.py
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.fixups_lib import lib_fixups
 from extract_utils.main import ExtractUtils
 
@@ -14,11 +18,17 @@ namespace_imports = [
     'vendor/qcom/common/system/telephony',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/init/vendor.qti.hardware.qccsyshal@1.2-service.rc': blob_fixup()
+        .regex_replace('qccsyshal@1.12-service', 'qccsyshal@1.2-service'),
+}  # fmt: skip
+
 module = ExtractUtilsQTIModule(
     'gps',
     QTIComponentType.SYSTEM,
-    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
--- system/gps/qti-gps.mk
+++ system/gps/qti-gps.mk
@@ -3,6 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Init
+PRODUCT_COPY_FILES += \
+    $(QCOM_COMMON_PATH)/system/gps/vendor.qti.hardware.qccsyshal@1.2-service.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/vendor.qti.hardware.qccsyshal@1.2-service_override.rc
+
 # Properties
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.backup.ntpServer="0.pool.ntp.org"
