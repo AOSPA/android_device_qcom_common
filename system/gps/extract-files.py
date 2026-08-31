d01864b987f877fbb37bf10d12aab1be0bacaf66
Omkar Chandorkar <gotenksIN@aospa.co>
2026-08-31 04:48:24 +0000

    system: Update to LA.QISI.17.0.r1-02700-qssi.0
    Change-Id: I64cbbe5a5e99f1e1808e403c64e7500e7d797f66
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- system/gps/extract-files.py
+++ system/gps/extract-files.py
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.fixups_lib import lib_fixups
 from extract_utils.main import ExtractUtils
 
@@ -14,11 +18,19 @@ namespace_imports = [
     'vendor/qcom/common/system/telephony',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/init/vendor.qti.hardware.qccsyshal@1.2-service.rc': blob_fixup()
+        .regex_replace('/system/system_ext/bin/qccsyshal@1.2-service', '/system_ext/bin/qccsyshal@1.2-service')
+        .regex_replace('group misc system', 'group misc system\\n    interface vendor.qti.hardware.qccsyshal@1.2::IQccsyshal qccsyshal\\n    interface vendor.qti.hardware.qccsyshal@1.1::IQccsyshal qccsyshal\\n    interface vendor.qti.hardware.qccsyshal@1.0::IQccsyshal qccsyshal')
+        .regex_replace('qccsyshal@1.12-service', 'qccsyshal@1.2-service'),
+}  # fmt: skip
+
 module = ExtractUtilsQTIModule(
     'gps',
     QTIComponentType.SYSTEM,
-    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
